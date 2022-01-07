eb0b4cce1c41730dd87d612b08c96081430baebc
Steven Degutis <sbdegutis@gmail.com>
2022-01-06 22:46:12 -0600

    Catch more errors.

--- src/imlib/updater.ts
+++ src/imlib/updater.ts
@@ -8,18 +8,20 @@ export class Updater {
   }
 
   create(data: object) {
-    if (typeof data !== 'object') throw new Error('updater.create() must be given object');
+    if (typeof data !== 'object') throw new Error('updater.create(...) must be given object');
     const serializable = JSON.parse(JSON.stringify(data));
     return this.#app.create(serializable);
   }
 
   update(id: string, data: object) {
-    if (typeof data !== 'object') throw new Error('updater.update() must be given object');
+    if (typeof id !== 'string') throw new Error('updater.update(...) must be given string id');
+    if (typeof data !== 'object') throw new Error('updater.update(...) must be given object');
     const serializable = JSON.parse(JSON.stringify(data));
     this.#app.put(id, serializable);
   }
 
   delete(id: string) {
+    if (typeof id !== 'string') throw new Error('updater.delete(...) must be given string id');
     this.#app.put(id, null);
   }
 
