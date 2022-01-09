50edfc6e0c0c637c26f28ca139647a50a6c2f13f
Steven Degutis <sbdegutis@gmail.com>
2022-01-08 21:07:08 -0600

    API

---
+++ src/imlib/api.d.ts
@@ -0,0 +1,57 @@
+import { URLSearchParams } from "url";
+import { Updater } from "./updater";
+
+module Api {
+
+  interface ItemData {
+
+    [key: string]: any | ItemFunction<any>;
+
+    $boot?: ItemFunction<(this: Item, site: {
+      items: Item[],
+      updater: Updater,
+      external: any,
+    }) => {
+      routes: Record<Route, RouteHandler>,
+      timers?: { ms: number, fn: Function }[],
+      notFoundPage?: RouteHandler,
+      onRouteError?: RouteHandler,
+    }>;
+
+  }
+
+  interface Item {
+    [key: string]: any;
+  }
+
+  interface ItemFunction<T extends (this: Item) => any> {
+    $eval: string & T;
+  }
+
+  type HttpVerb = Uppercase<string>;
+  type Route = `${HttpVerb} /${string}`
+
+  type RouteHandler = (input: RouteInput) => RouteOutput | Promise<RouteOutput>;
+
+  type RouteInput = {
+    path: () => string,
+    query: () => URLSearchParams,
+    text: () => string,
+    json: () => any,
+    form: () => URLSearchParams,
+    headers: () => unknown,
+    session: unknown,
+  };
+
+  type RouteOutput = ({
+    redirect: string;
+  } | {
+    text: any;
+  } | {
+    json: object;
+  }) & {
+    status?: number,
+    headers?: { [key: string]: string },
+  };
+
+}
