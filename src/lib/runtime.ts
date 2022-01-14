9f3929bdac9133a1d6711f78b6fc3ad689b2dc7f
Steven Degutis <sbdegutis@gmail.com>
2022-01-14 17:33:41 -0600

    rearrange

--- src/lib/runtime.ts
+++ src/lib/runtime.ts
@@ -3,70 +3,6 @@ import * as sucrase from 'sucrase';
 import vm from 'vm';
 import { Dir, File } from "./vfs";
 
-export class Module {
-
-  public exports = Object.create(null);
-  #ran = false;
-  #runtime: Runtime;
-
-  constructor(
-    public file: File,
-    runtime: Runtime,
-  ) {
-    this.#runtime = runtime;
-  }
-
-  require() {
-    if (!this.#ran) {
-      const rawCode = this.file.buffer.toString('utf8');
-
-      const args = {
-        require: (path: string) => this.#require(path),
-        exports: this.exports,
-        __dir: this.file.parent!,
-      };
-
-      const sucraseOptions: sucrase.Options = {
-        transforms: ['typescript', 'imports'],
-        disableESTransforms: true,
-        production: true,
-      };
-
-      if (this.#runtime.jsxCreateElement) {
-        (args as any)._JSX = {
-          createElement: this.#runtime.jsxCreateElement,
-          fragment: Symbol('fragment'),
-        };
-        sucraseOptions.transforms.push('jsx');
-        sucraseOptions.jsxPragma = '_JSX.createElement';
-        sucraseOptions.jsxFragmentPragma = '_JSX.fragment';
-      }
-
-      const { code } = sucrase.transform(rawCode, sucraseOptions);
-
-      const runModule = vm.compileFunction(code, Object.keys(args), {
-        filename: this.file.path,
-        parsingContext: this.#runtime.context,
-      });
-
-      runModule(...Object.values(args));
-
-      this.#ran = true;
-    }
-    return this.exports;
-  }
-
-  #require(toPath: string) {
-    const destPath = path.join(path.dirname(this.file.path), toPath);
-    const mod = this.#runtime.findModuleFromRoot(destPath);
-    if (!mod) {
-      throw new Error(`Can't find module at path: ${destPath}`);
-    }
-    return mod.require();
-  }
-
-}
-
 type JsxCreateElement = (
   tag: string | Function | symbol,
   attrs: any,
@@ -129,6 +65,70 @@ export class Runtime {
         this.modules.set(file, new Module(file, this));
       }
     }
+  }
+
+}
+
+class Module {
+
+  public exports = Object.create(null);
+  #ran = false;
+  #runtime: Runtime;
+
+  constructor(
+    public file: File,
+    runtime: Runtime,
+  ) {
+    this.#runtime = runtime;
+  }
+
+  require() {
+    if (!this.#ran) {
+      const rawCode = this.file.buffer.toString('utf8');
+
+      const args = {
+        require: (path: string) => this.#require(path),
+        exports: this.exports,
+        __dir: this.file.parent!,
+      };
+
+      const sucraseOptions: sucrase.Options = {
+        transforms: ['typescript', 'imports'],
+        disableESTransforms: true,
+        production: true,
+      };
+
+      if (this.#runtime.jsxCreateElement) {
+        (args as any)._JSX = {
+          createElement: this.#runtime.jsxCreateElement,
+          fragment: Symbol('fragment'),
+        };
+        sucraseOptions.transforms.push('jsx');
+        sucraseOptions.jsxPragma = '_JSX.createElement';
+        sucraseOptions.jsxFragmentPragma = '_JSX.fragment';
+      }
+
+      const { code } = sucrase.transform(rawCode, sucraseOptions);
+
+      const runModule = vm.compileFunction(code, Object.keys(args), {
+        filename: this.file.path,
+        parsingContext: this.#runtime.context,
+      });
+
+      runModule(...Object.values(args));
+
+      this.#ran = true;
+    }
+    return this.exports;
+  }
+
+  #require(toPath: string) {
+    const destPath = path.join(path.dirname(this.file.path), toPath);
+    const mod = this.#runtime.findModuleFromRoot(destPath);
+    if (!mod) {
+      throw new Error(`Can't find module at path: ${destPath}`);
+    }
+    return mod.require();
   }
 
 }
