f7bf85e690936cbd30e617f52c42dc6cab76b407
Steven Degutis <sbdegutis@gmail.com>
2022-01-26 21:48:43 -0600

    Starting on clone...

--- app/src/snippets/clone.tsx
+++ app/src/snippets/clone.tsx
@@ -1,6 +1,8 @@
 import { Snippet, snippetsById } from ".";
 import { Routeable } from "../core/router";
 import { guardAuth } from "../pages/admin";
+import { md } from "../util/helpers";
+import { Content } from "../view/components/page";
 import { Head, Html, SiteFooter, SiteHeader } from "../view/components/site";
 
 export function makeCloneRouteFor(snippet: Snippet) {
@@ -13,7 +15,7 @@ export const cloneSnippetPage: Routeable = {
   method: 'GET',
   handle: guardAuth((input) => {
 
-    const snippet = snippetsById.get(input.url.searchParams.get('id')!);
+    const snippet = snippetsById.get(input.url.searchParams.get('id')!)!;
 
     return {
       body: <>
@@ -22,7 +24,19 @@ export const cloneSnippetPage: Routeable = {
           <body>
             <SiteHeader />
             <main>
-              <div>Cloning...</div>
+              <div style='padding:1em;display:grid;gap:1em;grid-template-columns:1fr 1fr 1fr 1fr'>
+                <div>
+                  <Content>
+                    <h1>{snippet.title}</h1>
+                    {md.render(snippet.markdownContent)}
+                  </Content>
+                </div>
+                <div></div>
+                <div></div>
+                <div>
+                  <iframe style='width:100%;height:100%' src={snippet.archiveLink}></iframe>
+                </div>
+              </div>
             </main>
             <SiteFooter input={input} />
           </body>
