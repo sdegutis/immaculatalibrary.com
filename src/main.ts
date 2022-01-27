4b226441255708d448a1fe31241f53b01663c40f
Steven Degutis <sbdegutis@gmail.com>
2022-01-27 06:48:36 -0600

    Log timestamps.

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,22 @@ import 'source-map-support/register';
 import { startServer } from './http';
 import { Site } from './site';
 
+const formatter = new Intl.DateTimeFormat('en-US', {
+  timeStyle: 'medium',
+  dateStyle: 'short',
+  // fractionalSecondDigits: 3,
+});
+
+function wrapLog(key: 'log' | 'error') {
+  const realFn = console[key];
+  console[key] = (...args: any) => {
+    realFn(formatter.format(), '-', ...args);
+  };
+}
+
+wrapLog('log');
+wrapLog('error');
+
 const site = new Site();
 site.restartOnSourceChanges();
 
