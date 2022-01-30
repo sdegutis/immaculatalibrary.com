dcd3c49cd82ecd458f5e3173e44082fbd99f13c6
Steven Degutis <sbdegutis@gmail.com>
2022-01-30 09:39:49 -0600

    Move files into core.

---
+++ app/src/core/globals.d.ts
@@ -0,0 +1,3 @@
+declare const persisted: {
+  sessions: Map<string, import('../auth/login').Session>;
+};
--- app/src/persisted.d.ts
+++
@@ -1,3 +0,0 @@
-declare const persisted: {
-  sessions: Map<string, import('./auth/login').Session>;
-};
