a62f898e3c32a801cb4a70c3d91f4f33c1d6c772
Steven Degutis <sbdegutis@gmail.com>
2022-01-30 10:20:00 -0600

    Fixes.

--- app/components/content/content.tsx
+++ app/components/content/content.tsx
@@ -1,8 +1,8 @@
 import { staticRouteFor } from "../../util/static";
 
 export const Content: Component<{}> = (attrs, children) => <>
-  <link rel="stylesheet" href={staticRouteFor(__dir.filesByName["content.css"]!)} />
   <div class="content">
+    <link rel="stylesheet" href={staticRouteFor(__dir.filesByName["content.css"]!)} />
     {children}
   </div>
 </>;
