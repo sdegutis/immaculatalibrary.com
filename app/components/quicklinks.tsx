1d719e08d448cf3330211a13e4e32cfa21ea1625
Steven Degutis <sbdegutis@gmail.com>
2022-01-31 18:59:14 -0600

    Home page image sizes.

--- app/components/quicklinks.tsx
+++ app/components/quicklinks.tsx
@@ -21,7 +21,7 @@ export const QuickLinks: Component<{}> = (attrs, children) => {
           {recentPosts.map(post => <>
             <li class="post-box">
               <a href={post.view.route}>
-                <img class="image" src={post.imageSmall} alt={post.title} />
+                <img class="image" src={post.imageSmall} alt={post.title} width="1000" height="400" />
               </a>
               <a class="title" href={post.view.route}>
                 {post.title}
