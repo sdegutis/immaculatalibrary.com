a44d2a2a081108c983708cb81ff06aec16e2dfce
Steven Degutis <sbdegutis@gmail.com>
2022-01-31 19:20:40 -0600

    Woff fonts.

--- app/font/fonts.tsx
+++ app/font/fonts.tsx
@@ -1,15 +1,15 @@
 import { staticRouteFor } from "../util/static";
 
 export const inlineFontCss = `
-@font-face { font-display: block; font-family: "David Libre"; font-weight: 400; src: url('${staticRouteFor(__dir.filesByName['DavidLibre-Regular.ttf']!)}'); }
-@font-face { font-display: block; font-family: "David Libre"; font-weight: 600; src: url('${staticRouteFor(__dir.filesByName['DavidLibre-Medium.ttf']!)}'); }
-@font-face { font-display: block; font-family: "David Libre"; font-weight: 700; src: url('${staticRouteFor(__dir.filesByName['DavidLibre-Bold.ttf']!)}'); }
+@font-face { font-display: block; font-family: "David Libre"; font-weight: 400; src: url('${staticRouteFor(__dir.filesByName['DavidLibre-Regular.woff']!)}'); }
+@font-face { font-display: block; font-family: "David Libre"; font-weight: 600; src: url('${staticRouteFor(__dir.filesByName['DavidLibre-Medium.woff']!)}'); }
+@font-face { font-display: block; font-family: "David Libre"; font-weight: 700; src: url('${staticRouteFor(__dir.filesByName['DavidLibre-Bold.woff']!)}'); }
 
-@font-face { font-display: block; font-family: "Martel"; font-weight: 200; src: url('${staticRouteFor(__dir.filesByName['Martel-ExtraLight.ttf']!)}'); }
-@font-face { font-display: block; font-family: "Martel"; font-weight: 300; src: url('${staticRouteFor(__dir.filesByName['Martel-Light.ttf']!)}'); }
-@font-face { font-display: block; font-family: "Martel"; font-weight: 400; src: url('${staticRouteFor(__dir.filesByName['Martel-Regular.ttf']!)}'); }
-@font-face { font-display: block; font-family: "Martel"; font-weight: 600; src: url('${staticRouteFor(__dir.filesByName['Martel-SemiBold.ttf']!)}'); }
-@font-face { font-display: block; font-family: "Martel"; font-weight: 700; src: url('${staticRouteFor(__dir.filesByName['Martel-Bold.ttf']!)}'); }
-@font-face { font-display: block; font-family: "Martel"; font-weight: 800; src: url('${staticRouteFor(__dir.filesByName['Martel-ExtraBold.ttf']!)}'); }
-@font-face { font-display: block; font-family: "Martel"; font-weight: 900; src: url('${staticRouteFor(__dir.filesByName['Martel-Black.ttf']!)}'); }
+@font-face { font-display: block; font-family: "Martel"; font-weight: 200; src: url('${staticRouteFor(__dir.filesByName['Martel-ExtraLight.woff']!)}'); }
+@font-face { font-display: block; font-family: "Martel"; font-weight: 300; src: url('${staticRouteFor(__dir.filesByName['Martel-Light.woff']!)}'); }
+@font-face { font-display: block; font-family: "Martel"; font-weight: 400; src: url('${staticRouteFor(__dir.filesByName['Martel-Regular.woff']!)}'); }
+@font-face { font-display: block; font-family: "Martel"; font-weight: 600; src: url('${staticRouteFor(__dir.filesByName['Martel-SemiBold.woff']!)}'); }
+@font-face { font-display: block; font-family: "Martel"; font-weight: 700; src: url('${staticRouteFor(__dir.filesByName['Martel-Bold.woff']!)}'); }
+@font-face { font-display: block; font-family: "Martel"; font-weight: 800; src: url('${staticRouteFor(__dir.filesByName['Martel-ExtraBold.woff']!)}'); }
+@font-face { font-display: block; font-family: "Martel"; font-weight: 900; src: url('${staticRouteFor(__dir.filesByName['Martel-Black.woff']!)}'); }
 `;
