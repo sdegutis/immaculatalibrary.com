a705cf422b37cdfd14e23c9c8651277464f2d626
Steven Degutis <sbdegutis@gmail.com>
2022-02-13 16:17:23 -0600

    Remove report.html

--- app/main.tsx
+++ app/main.tsx
@@ -1,17 +1,7 @@
 import 'source-map-support/register';
 import { startServer } from './core/http';
 import './core/load';
-import { addRouteable, loadRoutes, makeRouteHandler } from './core/router';
-
-const routes = loadRoutes();
-
-addRouteable({
-  method: 'GET',
-  route: '/report.html',
-  handle: () => ({
-    body: __dir.filesByName['report.html']!.buffer
-  })
-});
+import { loadRoutes, makeRouteHandler } from './core/router';
 
 persisted.server ??= startServer(process.env['BASE_URL']!, 8080);
-persisted.server.handler = makeRouteHandler(routes);
+persisted.server.handler = makeRouteHandler(loadRoutes());
