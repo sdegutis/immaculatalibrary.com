dcc9a2e4bd354c00f0885c8dd520fd9f9cf06469
Steven Degutis <sbdegutis@gmail.com>
2022-02-13 17:06:18 -0600

    Moving load-routes file to top level.

--- app/main.tsx
+++ app/main.tsx
@@ -1,8 +1,8 @@
 import 'source-map-support/register';
 import { makeRouteHandler } from './core/handler';
 import { startServer } from './core/http';
-import './core/load';
 import { loadRoutes } from './core/router';
+import './load-route-files';
 
 persisted.server ??= startServer(process.env['BASE_URL']!, 8080);
 persisted.server.handler = makeRouteHandler(loadRoutes());
