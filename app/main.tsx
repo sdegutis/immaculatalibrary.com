bb439087feffeee84998d2c4fe5990c227ee07b2
Steven Degutis <sbdegutis@gmail.com>
2023-07-30 14:05:32 -0500

    Generate into /docs instead.

--- app/main.tsx
+++ app/main.tsx
@@ -9,9 +9,9 @@ import './load-route-files';
 const routes = loadRoutes();
 
 (async () => {
-  // await rimraf('site');
+  await rimraf('docs');
   for (const [route, handler] of routes) {
-    const filepath = path.join('site', route.slice(4));
+    const filepath = path.join('docs', route.slice(4));
     const body = handler().body!;
     console.log(filepath);
 
