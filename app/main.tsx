1e3bd66d68cb53d2acf7328c13c454c6661a6303
Steven Degutis <sbdegutis@gmail.com>
2023-07-30 14:00:54 -0500

    Generated static site.

--- app/main.tsx
+++ app/main.tsx
@@ -12,11 +12,10 @@ const routes = loadRoutes();
   // await rimraf('site');
   for (const [route, handler] of routes) {
     const filepath = path.join('site', route.slice(4));
-    console.log(filepath);
     const body = handler().body!;
-    console.log(filepath, body.length);
+    console.log(filepath);
 
-    // await mkdirp(path.dirname(filepath));
-    // fs.writeFileSync(filepath, body);
+    await mkdirp(path.dirname(filepath));
+    fs.writeFileSync(filepath, body);
   }
 })();
