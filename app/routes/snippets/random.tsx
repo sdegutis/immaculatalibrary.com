73ba3b140a4709cce4e72bd0610193dcd7f4b608
Steven Degutis <sbdegutis@gmail.com>
2023-07-30 11:47:40 -0500

    Client-side random snippet.

--- app/routes/snippets/random.tsx
+++ app/routes/snippets/random.tsx
@@ -1,15 +1,16 @@
+import { renderElement } from "../../core/jsx";
 import { addRouteable, Routeable } from "../../core/router";
 import { allSnippets } from "../../model/models";
-import { randomElement } from "../../util/helpers";
 
 export const randomSnippetPage: Routeable = {
   route: '/random-book-snippet.html',
   method: 'GET',
   handle: (input) => {
-    const snippet = randomElement(allSnippets);
     return {
-      status: 302,
-      headers: { 'Location': snippet.view.route },
+      body: renderElement(<>
+        <script>{`const pages = ${JSON.stringify(allSnippets.map(snippet => snippet.view.route))}`}</script>
+        <script>{`const i = Math.floor(Math.random() * pages.length); window.location = pages[i];`}</script>
+      </>),
     }
   },
 };
