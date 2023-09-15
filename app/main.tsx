c5ac7e8349d91b9a5d97810ec9f805c321e0fa97
Steven Degutis <sbdegutis@gmail.com>
2023-09-15 18:53:18 -0500

    Not gonna need these anymore.

--- app/main.tsx
+++ app/main.tsx
@@ -16,16 +16,16 @@ server.httpHandler = makeRequestHandler(makeRouteHandler(routes));
 // generateSite();
 
 async function generateSite() {
-  await rimraf('docs');
+  // await rimraf('docs');
   for (const [route, handler] of routes) {
     const filepath = path.join('docs', route);
     const body = handler().body!;
-    console.log(filepath);
+    // console.log(filepath);
 
-    await mkdirp(path.dirname(filepath));
-    fs.writeFileSync(filepath, body);
+    // await mkdirp(path.dirname(filepath));
+    // fs.writeFileSync(filepath, body);
   }
-  fs.writeFileSync('docs/CNAME', 'www.immaculatalibrary.com');
+  // fs.writeFileSync('docs/CNAME', 'www.immaculatalibrary.com');
 
   console.log('Done generating site.');
 };
