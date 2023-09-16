d466cd35692b9428f1a2f36b5603583c7290544d
Steven Degutis <sbdegutis@gmail.com>
2023-09-16 17:56:52 -0500

    Using typography component.

---
+++ app/components/new/typography.tsx
@@ -0,0 +1,6 @@
+export const Typography: JSX.Component<{}> = (attrs, children) => <>
+  <div class="typography">
+    <link rel="stylesheet" href='/css/typography.css' />
+    {children}
+  </div>
+</>;
---
+++ app/site/about.html.tsx
@@ -0,0 +1,44 @@
+import { Html } from "../components/new/html";
+import { Typography } from "../components/new/typography";
+
+export default <>
+  <Html>
+    <body>
+      <main>
+
+        {/* <HeroImage image={attrs.image}>
+          <SiteHeader />
+        </HeroImage>
+
+        <Navlinks /> */}
+
+        {/* <Container spaced centered> */}
+        <Typography>
+
+          <h1>About Immaculata Library</h1>
+
+          <p>The website Immaculata Library began as a quick place
+            to store digital copies of invaluable and timeless
+            Catholic books that have become copyright free,
+            in order to easily share them with friends and family.</p>
+
+          <p>Over time, it has grown to be a full online library,
+            with links to free and paid Sacred music, links and
+            reviews of Catholic movies, and links to other resources
+            to help Catholics grow in devotion in this digital age.</p>
+
+          <p>Only the most useful and approved of all Catholic books
+            are selected for this website. This means, only books that
+            have received official approbations from Bishops, *and* have
+            helped to produce Saints, or are written by Saints, are offered.</p>
+
+        </Typography>
+        {/* </Container> */}
+
+      </main>
+
+      {/* <QuickLinks />
+      <SiteFooter /> */}
+    </body>
+  </Html>
+</>;
