be872b7d70050bb6ac62d2637bedbef6f8a6f4fe
Steven Degutis <sbdegutis@gmail.com>
2023-09-16 18:40:04 -0500

    Get ready for quicklinks.

---
+++ app/components/new/quicklinks.tsx
@@ -0,0 +1,44 @@
+// import { allCategories, allMovies } from "../model/models";
+
+// export const QuickLinks: JSX.Component<{}> = (attrs, children) => {
+//   return <>
+//     <link rel="stylesheet" href='/css/quicklinks.css' />
+
+//     <div id="recents">
+//       <div class="container">
+
+//         <h2>Books</h2>
+//         <ul class="quicklinks">
+
+//           {allCategories.map(cat => <li>
+//             <a class="link" href={cat.view.route} style={`background-image: url(${cat.imageSmall});`}>
+//               <span>{cat.shortTitle}</span>
+//             </a>
+//           </li>
+//           )}
+
+//         </ul>
+
+//         <h2>Movies</h2>
+//         <ul class="quicklinks">
+
+//           {allMovies.map(movie => <li>
+//             <a class="link" href={movie.view.route} style={`background-image: url(${movie.smallImage});`}>
+//               <span>{movie.shortTitle}</span>
+//             </a>
+//           </li>
+//           )}
+
+//           <li>
+//             <a class="link" href='/audio-bible.html' style={`background-image: url(/img/page/audiobible-small.jpg);`}>
+//               <span>Audio Bible</span>
+//             </a>
+//           </li>
+
+//         </ul>
+
+//       </div>
+//     </div>
+
+//   </>;
+// };
