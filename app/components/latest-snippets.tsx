7b88b71dfa58f59c5603ab796b4c8a2894ea1584
Steven Degutis <sbdegutis@gmail.com>
2023-09-17 11:55:15 -0500

    Starting on snippet groups component.

--- app/components/latest-snippets.tsx
+++ app/components/latest-snippets.tsx
@@ -2,43 +2,6 @@
 // import { allSnippets } from "../model/models";
 // import { Snippet } from "../model/snippets";
 
-// export const SnippetsGroups: JSX.Component<{ groups: [string, Snippet[]][] }> = ({ groups }, children) => <>
-//   <style>{`
-//     ul.snippets-latest {
-//       padding-left: 0;
-//     }
-
-//     ul.snippets-latest > li {
-//       list-style-type: none;
-//     }
-
-//     ul.snippets-latest > li ul {
-//       padding-left: 20px;
-//     }
-
-//     ul.snippets-latest > li li {
-//       list-style-type: disc;
-//     }
-//   `}</style>
-//   <ul class="snippets-latest">
-//     {groups.map(([date, group]) => <>
-//       <li>
-//         <h4>{date}</h4>
-//         <ul>
-//           {group.map(snippet => <>
-//             <li>
-//               <p>
-//                 <a href={snippet.route}>{snippet.renderedTitle}</a>
-//                 <br /> {calculateReadingMins(snippet.content)} min &mdash; {snippet.book.title}
-//               </p>
-//             </li>
-//           </>)}
-//         </ul>
-//       </li>
-//     </>)}
-//   </ul>
-// </>;
-
 // const totalReadingMins = calculateReadingMins(allSnippets.map(s => s.content).join('\n\n'));
 // const mins = Math.round(totalReadingMins % 60);
 // const hours = Math.floor(totalReadingMins / 60);
