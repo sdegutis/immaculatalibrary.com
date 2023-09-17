f70cb250cee0ab8a358478fd4a7fbb6b94f8a7b6
Steven Degutis <sbdegutis@gmail.com>
2023-09-17 07:05:51 -0500

    Adding music pages.

---
+++ app/site/music/musics.html.tsx
@@ -0,0 +1,39 @@
+import * as Common from "../../components/common";
+import { MusicSidebar } from "../../components/music-sidebar";
+import { allMusics } from "../../model/models";
+import { markdown } from "../../util/helpers";
+
+export default allMusics.map(song => {
+  const embedUrl = song.youtube.replace('watch?v=', 'embed/').replace(/&t=(\d+)s/, '?start=$1');
+  return [`${song.slug}.html`, <>
+    <link rel="stylesheet" href='/css/view-song.css' />
+
+    <Common.Page>
+
+      <Common.SiteHeader image='/img/page/music.jpg' />
+      <Common.Navlinks />
+
+      <main>
+
+        <Common.Column spaced split>
+
+          <Common.Typography>
+            <h1>{song.title}</h1>
+            <div class="embed-container">
+              <iframe allowfullscreen="allowfullscreen" frameborder="0" src={embedUrl}></iframe>
+            </div>
+            {markdown.render(song.content)}
+          </Common.Typography>
+
+          <MusicSidebar />
+
+        </Common.Column>
+
+      </main>
+
+      <Common.QuickLinks />
+      <Common.SiteFooter />
+
+    </Common.Page>
+  </>];
+});
--- old-routes/music/content.md
+++
@@ -1,1 +0,0 @@
-Reading holy books can help us grow in devotion to God, and movies can help us visualize these sacred mysteries. But countless angels are singing the Lord's praises *right now* in Heaven. Listening to Sacred Music (and singing or playing it!) is a powerful way to join these angels in praising our Lord and Redeemer, in a way that deeply touches the Father's heart.
