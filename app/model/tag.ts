b55ca6180c06565c5f00eddef4f82dd0fe2208cc
Steven Degutis <sbdegutis@gmail.com>
2023-09-17 06:44:06 -0500

    Commenting out for now.

--- app/model/tag.ts
+++ app/model/tag.ts
@@ -1,41 +1,41 @@
-import { ViewTagRoute } from "../routes/snippets/tag";
-import { sortBy } from "../util/helpers";
-import { Snippet } from "./snippet";
+// import { ViewTagRoute } from "../routes/snippets/tag";
+// import { sortBy } from "../util/helpers";
+// import { Snippet } from "./snippet";
 
-export const allTags = new Map<string, Tag>();
+// export const allTags = new Map<string, Tag>();
 
-export class Tag {
+// export class Tag {
 
-  snippets = new Set<Snippet>();
+//   snippets = new Set<Snippet>();
 
-  static getOrCreate(name: string) {
-    let tag = allTags.get(name);
-    if (!tag) allTags.set(name, tag = new Tag(name));
-    return tag;
-  }
+//   static getOrCreate(name: string) {
+//     let tag = allTags.get(name);
+//     if (!tag) allTags.set(name, tag = new Tag(name));
+//     return tag;
+//   }
 
-  view;
-  slug;
+//   view;
+//   slug;
 
-  constructor(public name: string) {
-    this.slug = this.name.split(' ').map(capitalize).join('');
-    this.view = new ViewTagRoute(this);
-  }
+//   constructor(public name: string) {
+//     this.slug = this.name.split(' ').map(capitalize).join('');
+//     this.view = new ViewTagRoute(this);
+//   }
 
-  addSnippet(snippet: Snippet) {
-    this.snippets.add(snippet);
-  }
+//   addSnippet(snippet: Snippet) {
+//     this.snippets.add(snippet);
+//   }
 
-  removeSnippet(snippet: Snippet) {
-    this.snippets.delete(snippet);
-  }
+//   removeSnippet(snippet: Snippet) {
+//     this.snippets.delete(snippet);
+//   }
 
-}
+// }
 
-export function sortedTags() {
-  return [...allTags.values()].sort(sortBy(tag => tag.name));
-}
+// export function sortedTags() {
+//   return [...allTags.values()].sort(sortBy(tag => tag.name));
+// }
 
-function capitalize(str: string) {
-  return str[0]?.toUpperCase() + str.slice(1);
-}
+// function capitalize(str: string) {
+//   return str[0]?.toUpperCase() + str.slice(1);
+// }
