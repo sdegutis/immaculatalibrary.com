42b8938048615675b26aa020bcad1af3b48765d2
Steven Degutis <sbdegutis@gmail.com>
2023-09-17 06:56:18 -0500

    Article previews.

--- app/model/articles.ts
+++ app/model/articles.ts
@@ -1,5 +1,7 @@
 import { loadContentFile } from '../util/data-files';
 
+const PREVIEW_LENGTH = 2000;
+
 export interface Article {
   date: string;
   slug: string;
@@ -7,24 +9,27 @@ export interface Article {
 
   title: string;
   draft?: boolean;
+
+  previewMarkdown: string | null;
 }
 
 export function articleFromFile(file: FsFile) {
   const data = loadContentFile<Article>(file);
+  data.previewMarkdown = derivePreview(data);
   return data;
 }
 
-// private derivePreview(count = 2000) {
-//   const paragraphs = this.markdownContent.trim().split(/(\r?\n>+ *\r?\n)/);
+function derivePreview(article: Article) {
+  const paragraphs = article.content.trim().split(/(\r?\n>+ *\r?\n)/);
 
-//   let running = 0;
-//   for (let i = 0; i < paragraphs.length; i++) {
-//     running += paragraphs[i]!.length;
-//     if (running > count) break;
-//   }
+  let running = 0;
+  for (let i = 0; i < paragraphs.length; i++) {
+    running += paragraphs[i]!.length;
+    if (running > PREVIEW_LENGTH) break;
+  }
 
-//   if (running < this.markdownContent.length - 1) {
-//     return this.markdownContent.substring(0, running);
-//   }
-//   return null;
-// }
+  if (running < article.content.length - 1) {
+    return article.content.substring(0, running);
+  }
+  return null;
+}
