d3a6e125664aacf266acd35c55f3077ea3fd0a61
Steven Degutis <sbdegutis@gmail.com>
2023-09-16 20:31:03 -0500

    Trying out smaller import idea.

---
+++ app/components/new/common.tsx
@@ -0,0 +1,9 @@
+export { Column } from "./column";
+export { MoviesSidebar } from "./movies-sidebar";
+export { Navlinks } from "./navlinks";
+export { Page } from "./page";
+export { QuickLinks } from "./quicklinks";
+export { SiteFooter } from "./site-footer";
+export { SiteHeader } from "./site-header";
+export { Typography } from "./typography";
+
--- app/site/movies/movie.tsx
+++ app/site/movies/movie.tsx
@@ -1,37 +1,30 @@
-import { Column } from "../../components/new/column";
-import { MoviesSidebar } from "../../components/new/movies-sidebar";
-import { Navlinks } from "../../components/new/navlinks";
-import { Page } from "../../components/new/page";
-import { QuickLinks } from "../../components/new/quicklinks";
-import { SiteFooter } from "../../components/new/site-footer";
-import { SiteHeader } from "../../components/new/site-header";
-import { Typography } from "../../components/new/typography";
+import * as Common from "../../components/new/common";
 import { allMovies } from "../../model/new/movies";
 import { markdown } from "../../util/helpers";
 
 export default allMovies.map(movie => {
   return [`${movie.slug}.html`, <>
-    <Page>
+    <Common.Page>
       <main>
 
-        <SiteHeader image={`/img/movies/${movie.slug}-big.jpg`} />
-        <Navlinks />
+        <Common.SiteHeader image={`/img/movies/${movie.slug}-big.jpg`} />
+        <Common.Navlinks />
 
-        <Column spaced split>
-          <Typography>
+        <Common.Column spaced split>
+          <Common.Typography>
 
             <h1>{movie.displayTitle}</h1>
             {movie.meta.subtitle && <h4><i>{movie.meta.subtitle}</i></h4>}
             {markdown.render(movie.markdownContent)}
 
-          </Typography>
-          <MoviesSidebar />
-        </Column>
+          </Common.Typography>
+          <Common.MoviesSidebar />
+        </Common.Column>
 
       </main>
 
-      <QuickLinks />
-      <SiteFooter />
-    </Page>
+      <Common.QuickLinks />
+      <Common.SiteFooter />
+    </Common.Page>
   </>];
 });
