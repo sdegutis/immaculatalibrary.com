aab95c0b60ceece2748bd4c277f5fc90a531bcb6
Steven Degutis <sbdegutis@gmail.com>
2023-09-17 06:29:47 -0500

    Sorting and filtering models.

--- app/model/new/models.ts
+++ app/model/new/models.ts
@@ -1,9 +1,4 @@
-import articlesDir from '../../data/articles/';
-import booksDir from '../../data/books/';
-import categoriesDir from '../../data/categories/';
-import moviesDir from '../../data/movies/';
-import musicDir from '../../data/music/';
-import snippetsDir from '../../data/snippets/';
+import { sortBy } from '../../util/helpers';
 import { articleFromFile } from "./articles";
 import { bookFromFile } from "./books";
 import { categoryFromFile, categorySorter } from "./categories";
@@ -11,9 +6,29 @@ import { movieFromFile, movieSorter } from "./movies";
 import { musicFromFile } from './musics';
 import { snippetFromFile } from './snippets';
 
-export const allMovies = moviesDir.files.map(movieFromFile).sort(movieSorter);
-export const allArticles = articlesDir.files.map(articleFromFile);
-export const allBooks = booksDir.files.map(bookFromFile);
-export const allCategories = categoriesDir.files.map(categoryFromFile).sort(categorySorter);
-export const allMusics = musicDir.files.map(musicFromFile);
-export const allSnippets = snippetsDir.files.map(snippetFromFile);
+export const allMovies = ((require('/data/movies/') as FsDir).files
+  .map(movieFromFile)
+  .sort(movieSorter));
+
+export const allArticles = ((require('/data/articles/') as FsDir).files
+  .map(articleFromFile)
+  .sort(sortBy(article => article.date))
+  .filter(s => !s.draft)
+  .reverse());
+
+export const allBooks = ((require('/data/books/') as FsDir).files
+  .map(bookFromFile)
+  .sort(sortBy(b => `${b.dateAdded} ${b.slug}`)));
+
+export const allCategories = ((require('/data/categories/') as FsDir).files
+  .map(categoryFromFile)
+  .sort(categorySorter));
+
+export const allMusics = ((require('/data/music/') as FsDir).files
+  .map(musicFromFile));
+
+export const allSnippets = ((require('/data/snippets/') as FsDir).files
+  .map(snippetFromFile)
+  .filter((s => s.published))
+  .sort(sortBy(s => s.slug))
+  .reverse());
