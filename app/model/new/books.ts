cd70be8e9eee74c32a642a50bc45cdaf13999cb1
Steven Degutis <sbdegutis@gmail.com>
2023-09-17 00:00:50 -0500

    Books and articles are done.

---
+++ app/model/new/books.ts
@@ -0,0 +1,26 @@
+import booksDir from '../../data/books/';
+import { loadContentFile } from '../../util/data-files';
+
+export const allBooks = booksDir.files.map(file => {
+  const slug = file.name.slice(0, -3);
+  const data = loadContentFile<{
+    title: string,
+    subtitle: string,
+    dateAdded: string,
+    author: string,
+    translator: string,
+    score: number,
+    rating: number,
+    files: {
+      archiveId: string,
+      pdfFile: string,
+    }[],
+    storeLinks: {
+      link: string;
+      image: string;
+      title: string;
+    }[],
+    complete?: boolean,
+  }>(file, 'slug');
+  return { ...data, slug };
+});
