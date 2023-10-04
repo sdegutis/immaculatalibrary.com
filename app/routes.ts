089bf48b1853fb84dd929ea1f50409c175c60e4e
Steven Degutis <sbdegutis@gmail.com>
2023-10-04 07:10:34 -0500

    Fix bug with resource caching.

--- app/routes.ts
+++ app/routes.ts
@@ -3,6 +3,6 @@ export const out = new Map<string, Buffer | string>();
 export const resources = new Map<string, Buffer>();
 
 export function staticRouteFor([filepath, buffer]: [string, Buffer]) {
-  if (!resources.has(filepath)) resources.set(filepath, buffer);
+  resources.set(filepath, buffer);
   return filepath;
 }
