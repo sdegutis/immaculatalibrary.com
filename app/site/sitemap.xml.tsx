ace97b1d5dc8ec4281dc1a607d1d5fa1f8c99c8f
Steven Degutis <sbdegutis@gmail.com>
2023-10-05 22:40:25 -0500

    Redundant now.

--- app/site/sitemap.xml.tsx
+++ app/site/sitemap.xml.tsx
@@ -8,7 +8,7 @@ export default <>
     'xsi:schemaLocation': "http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd",
     'xmlns': "http://www.sitemaps.org/schemas/sitemap/0.9",
   }}>
-    {[...out.keys()].filter(filepath => !filepath.startsWith('/admin')).map(filepath => {
+    {[...out.keys()].map(filepath => {
       const name = path.basename(filepath);
       const date = name.match(/^(\d{4}-\d{2}-\d{2})-/)?.[1];
       return <>
