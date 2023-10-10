91c608c452618270abcad3079d0a1154f643bc31
Steven Degutis <sbdegutis@gmail.com>
2023-10-10 00:05:13 -0500

    Oops.

--- site/components/darkmode/dark-mode.tsx
+++ site/components/darkmode/dark-mode.tsx
@@ -1,7 +1,5 @@
 import script from './darkmode.js';
 
-console.log(script.path)
-
 export const DarkModeButton: JSX.Component = (attrs, children) => <>
   <script src={script.path} defer></script>
   <a href="#" class="dark-mode-toggle">Dark mode</a>
