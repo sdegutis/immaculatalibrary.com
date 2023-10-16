a447397197a797ad7f51512e75ad75923e85b590
Steven Degutis <sbdegutis@gmail.com>
2023-10-15 19:06:18 -0500

    Preserve current random snippet on home page in browser history.

--- site/script/random-book-snippet.js
+++ site/script/random-book-snippet.js
@@ -5,10 +5,17 @@ document.getElementById('refresh-random-book-snippet').addEventListener('click',
   doRandomBookSnippet();
 });
 
-async function doRandomBookSnippet() {
-  const slugs = await snippets;
-  const i = Math.floor(Math.random() * slugs.length);
-  const slug = slugs[i];
+window.addEventListener('popstate', e => {
+  reflectUrl();
+});
+
+if (!window.location.hash) {
+  doRandomBookSnippet();
+}
+
+async function reflectUrl() {
+  const slug = window.location.hash.slice(1);
+  if (!slug) return;
 
   const text = await fetch(`/dynamic/snippets/${slug}-preview.html`).then(res => res.text());
   const container = document.getElementById('random-book-snippet');
@@ -23,4 +30,11 @@ async function doRandomBookSnippet() {
   });
 }
 
-doRandomBookSnippet();
+async function doRandomBookSnippet() {
+  const slugs = await snippets;
+  const i = Math.floor(Math.random() * slugs.length);
+  const slug = slugs[i];
+
+  history.pushState('', '', '#' + slug);
+  reflectUrl();
+}
