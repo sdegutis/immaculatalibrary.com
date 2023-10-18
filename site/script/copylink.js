c56515419a0bb2168e3d7785a44ce503010d8015
Steven Degutis <sbdegutis@gmail.com>
2023-10-18 12:34:56 -0500

    Better copy link animation.

--- site/script/copylink.js
+++ site/script/copylink.js
@@ -4,10 +4,12 @@ function copylink(e) {
   navigator.clipboard.writeText(a.href);
 
   const done = document.createElement('span');
-  done.innerHTML = ' (Copied)';
+  done.innerHTML = 'Link copied.';
   a.parentElement.insertAdjacentElement('beforeend', done);
+  a.hidden = true;
 
   setTimeout(() => {
+    a.hidden = false;
     done.remove();
   }, 3000);
 }
