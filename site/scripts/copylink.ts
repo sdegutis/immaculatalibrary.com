192973031d7c030ff2caf7c956f5e0d5e4a8f4c3
Steven Degutis <sbdegutis@gmail.com>
2023-10-24 09:29:30 -0500

    Typing copy link script.

--- site/scripts/copylink.ts
+++ site/scripts/copylink.ts
@@ -1,6 +1,6 @@
-function copylink(e) {
+function copylink(e: Event) {
   e.preventDefault();
-  const a = e.target;
+  const a = e.target as HTMLAnchorElement;
   navigator.clipboard.writeText(a.href);
 
   const done = document.createElement('span');
