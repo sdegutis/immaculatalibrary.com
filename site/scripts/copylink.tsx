abe5971b48d4b87b971275aa412891c4dbd62c1a
Steven Degutis <sbdegutis@gmail.com>
2023-10-26 16:53:51 -0500

    Using JSX in client.

--- site/scripts/copylink.tsx
+++ site/scripts/copylink.tsx
@@ -1,11 +1,20 @@
+function createTree(spec: JSX.Element) {
+  const el = document.createElement(spec.tag);
+  for (const child of spec.children) {
+    if (typeof child === 'string') {
+      el.append(child);
+    }
+  }
+  return el;
+}
+
 for (const a of document.querySelectorAll<HTMLAnchorElement>('.copylink')) {
   a.addEventListener('click', e => {
     e.preventDefault();
 
     navigator.clipboard.writeText(a.href);
 
-    const done = document.createElement('span');
-    done.innerHTML = 'Link copied.';
+    const done = createTree(<span>Link copied.</span>);
     a.insertAdjacentElement('afterend', done);
     a.hidden = true;
 
