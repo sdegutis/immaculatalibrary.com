d171f38eb94affcf0d925d4b310dc813896006a7
Steven Degutis <sbdegutis@gmail.com>
2023-10-28 21:52:30 -0500

    Fix the last commit.

--- src/generate.ts
+++ src/generate.ts
@@ -2,27 +2,31 @@ import * as fs from 'fs';
 import * as path from 'path/posix';
 import { Runtime } from './runtime.js';
 
-const runtime = new Runtime("site");
-await runtime.setup();
+(async () => {
 
-const out = (await runtime.build())!.outfiles;
+  const runtime = new Runtime("site");
+  await runtime.setup();
 
-const madeDirs = new Set<string>();
-const mkdirIfNeeded = (dir: string) => {
-  if (madeDirs.has(dir)) return;
-  madeDirs.add(dir);
-  console.log('mkdir', dir);
-  fs.mkdirSync(dir);
-};
+  const out = (await runtime.build())!.outfiles;
 
-for (const [filepath, content] of out) {
-  const newFilepath = path.join('docs', filepath);
-  const parts = newFilepath.split(path.sep);
-  for (let i = 1; i < parts.length; i++) {
-    const dir = path.join(...parts.slice(0, i));
-    mkdirIfNeeded(dir);
+  const madeDirs = new Set<string>();
+  const mkdirIfNeeded = (dir: string) => {
+    if (madeDirs.has(dir)) return;
+    madeDirs.add(dir);
+    console.log('mkdir', dir);
+    fs.mkdirSync(dir);
+  };
+
+  for (const [filepath, content] of out) {
+    const newFilepath = path.join('docs', filepath);
+    const parts = newFilepath.split(path.sep);
+    for (let i = 1; i < parts.length; i++) {
+      const dir = path.join(...parts.slice(0, i));
+      mkdirIfNeeded(dir);
+    }
+
+    console.log('writefile', newFilepath)
+    fs.writeFileSync(newFilepath, content);
   }
 
-  console.log('writefile', newFilepath)
-  fs.writeFileSync(newFilepath, content);
-}
+})();
