312f75380a3834e8f1b2ada658743204889f20cf
Steven Degutis <sbdegutis@gmail.com>
2023-11-01 11:35:27 -0500

    Simpler footer.

--- site/components/site-footer.tsx
+++ site/components/site-footer.tsx
@@ -1,10 +1,6 @@
 export const SiteFooter: JSX.Component = (attrs, children) => <>
   <footer id='site-footer'>
     <link rel="stylesheet" href='/css/components/site-footer.css' />
-    <p>
-      <span>2020-{new Date().getFullYear()} Steven Degutis &copy; All Rights Reserved</span>
-      <> &bull; </>
-      <a href="mailto:sbdegutis@gmail.com">Contact</a>
-    </p>
+    <p><span>2020-{new Date().getFullYear()} <a href="mailto:sbdegutis@gmail.com">Steven Degutis</a> &copy; All Rights Reserved</span></p>
   </footer>
 </>;
