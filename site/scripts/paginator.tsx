0028480dd75881750bd092161f91cf6c13395de4
Steven Degutis <sbdegutis@gmail.com>
2023-11-12 08:13:36 -0600

    Reusing paginator logic in both places.

---
+++ site/scripts/paginator.tsx
@@ -0,0 +1,63 @@
+import { Reactive, reactTo } from "./reactive.js";
+
+export function makePaginator(
+  matchingItems: Reactive<any[]>,
+  perPage: number,
+) {
+  const page = new Reactive(0);
+
+  const highestPage = Reactive.from({ matchingItems, }, (deps) => {
+    return Math.max(0, Math.floor((deps.matchingItems.val.length - 1) / perPage));
+  });
+
+  const matchingCount = Reactive.from({ matchingItems }, deps => deps.matchingItems.val.length);
+
+  const prevButton = <button
+    style='width:2em'
+    onclick={(e: Event) => {
+      e.preventDefault();
+      page.set(Math.max(0, page.val - 1));
+    }}
+  >&lsaquo;</button> as HTMLButtonElement;
+
+  const nextButton = <button
+    style='width:2em'
+    onclick={(e: Event) => {
+      e.preventDefault();
+      page.set(Math.min(highestPage.val, page.val + 1));
+    }}
+  >&rsaquo;</button> as HTMLButtonElement;
+
+  reactTo({ page, highestPage }, deps => {
+    nextButton.toggleAttribute('disabled', deps.page.val === deps.highestPage.val);
+  });
+
+  reactTo({ page }, deps => {
+    prevButton.toggleAttribute('disabled', deps.page.val === 0);
+  });
+
+  const currentPage = <span style='min-width:7em; text-align:center' /> as HTMLSpanElement;
+  reactTo({ page, matchingCount }, deps => {
+    if (deps.matchingCount.val === 0) {
+      currentPage.textContent = 'n/a';
+      return;
+    }
+
+    const start = (deps.page.val * perPage) + 1;
+    const end = Math.min(deps.matchingCount.val, start + perPage - 1);
+    currentPage.textContent = `${start} - ${end} / ${deps.matchingCount.val}`;
+  });
+
+  const visibleItems = Reactive.from({ matchingItems, page }, deps => {
+    const start = deps.page.val * perPage;
+    return deps.matchingItems.val.slice(start, start + perPage);
+  });
+
+  return {
+    page,
+    visibleItems,
+    controls: <p style='display:flex; gap:1em; align-items:baseline'>
+      {prevButton} {currentPage} {nextButton}
+    </p> as HTMLParagraphElement,
+  };
+}
--- site/scripts/searchlist.tsx
+++ site/scripts/searchlist.tsx
@@ -1,3 +1,4 @@
+import { makePaginator } from "./paginator.js";
 import { Reactive, reactTo } from "./reactive.js";
 
 export interface SearchFilter<T> {
@@ -5,12 +6,12 @@ export interface SearchFilter<T> {
   matches: (data: T) => boolean,
 }
 
-const NotFound: JSX.Component<{ visibleCount: Reactive<number> }> = ({
-  visibleCount,
+const NotFound: JSX.Component<{ matchingItems: Reactive<any[]> }> = ({
+  matchingItems,
 }) => {
   const notFound = <em>No results</em> as HTMLElement;
-  reactTo({ visibleCount }, deps => {
-    notFound.hidden = deps.visibleCount.val !== 0;
+  reactTo({ matchingItems }, deps => {
+    notFound.hidden = deps.matchingItems.val.length !== 0;
   });
   return <>{notFound}</>;
 }
@@ -36,61 +37,15 @@ export function createSearch<T>({ data, Item, filters, perPage = 7 }: {
   perPage?: number,
 }) {
   const matchingItems = new Reactive<T[]>([]);
-  const page = new Reactive(0);
 
-  const matchingCount = Reactive.from({ matchingItems }, deps => deps.matchingItems.val.length);
-
-  const visibleItems = Reactive.from({ matchingItems, page }, deps => {
-    const start = deps.page.val * perPage;
-    return deps.matchingItems.val.slice(start, start + perPage);
-  });
-
-  const highestPage = Reactive.from({ matchingItems, }, (deps) => {
-    return Math.max(0, Math.floor((deps.matchingItems.val.length - 1) / perPage));
-  });
-
-  const prevButton = <button
-    style='width:2em'
-    onclick={(e: Event) => {
-      e.preventDefault();
-      page.set(Math.max(0, page.val - 1));
-    }}
-  >&lsaquo;</button> as HTMLButtonElement;
-
-  const nextButton = <button
-    style='width:2em'
-    onclick={(e: Event) => {
-      e.preventDefault();
-      page.set(Math.min(highestPage.val, page.val + 1));
-    }}
-  >&rsaquo;</button> as HTMLButtonElement;
-
-  reactTo({ page, highestPage }, deps => {
-    nextButton.toggleAttribute('disabled', deps.page.val === deps.highestPage.val);
-  });
-
-  reactTo({ page }, deps => {
-    prevButton.toggleAttribute('disabled', deps.page.val === 0);
-  });
-
-  const currentPage = <span style='min-width:7em; text-align:center' /> as HTMLSpanElement;
-  reactTo({ page, matchingCount }, deps => {
-    if (deps.matchingCount.val === 0) {
-      currentPage.textContent = 'n/a';
-      return;
-    }
-
-    const start = (deps.page.val * perPage) + 1;
-    const end = Math.min(deps.matchingCount.val, start + perPage - 1);
-    currentPage.textContent = `${start} - ${end} / ${deps.matchingCount.val}`;
-  });
+  const paginator = makePaginator(matchingItems, perPage);
 
   const results = <>
-    <p style='display:flex; gap:1em; align-items:baseline'>{prevButton} {currentPage} {nextButton}</p>
-    <NotFound visibleCount={matchingCount} />
+    {paginator.controls}
+    <NotFound matchingItems={matchingItems} />
     <ul>
       {data.map(item =>
-        <LiveItem item={item} visibleItems={visibleItems} Item={Item} />
+        <LiveItem item={item} visibleItems={paginator.visibleItems} Item={Item} />
       )}
     </ul>
   </>;
@@ -99,14 +54,14 @@ export function createSearch<T>({ data, Item, filters, perPage = 7 }: {
     matchingItems.set(data.filter(item => filters.every(filter => filter.matches(item))));
   };
 
-  page.onChange(updateMatchingItems);
+  paginator.page.onChange(updateMatchingItems);
 
   for (const filter of filters) {
     filter.source.onChange(() => {
-      page.set(0);
+      paginator.page.set(0);
       updateMatchingItems();
     });
   }
 
-  return { results, matchingCount, matchingItems };
+  return { results, matchingItems };
 }
