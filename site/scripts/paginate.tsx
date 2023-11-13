189e79c90d13c3c6078c4597e6897306f781bb95
Steven Degutis <sbdegutis@gmail.com>
2023-11-12 18:34:23 -0600

    Simple way to keep headers continuous in pagination.

--- site/scripts/paginate.tsx
+++ site/scripts/paginate.tsx
@@ -10,15 +10,41 @@ function enablePagination(topParent: HTMLElement, perPage: number) {
   loaders!.remove();
   parent!.hidden = false;
 
-  const items = [...parent!.children] as HTMLElement[];
+  const items: HTMLElement[] = [];
+  const headers = new Map<HTMLElement, HTMLElement>();
+
+  let lastHeader;
+
+  for (const child of parent!.children as Iterable<HTMLElement>) {
+    if (child.tagName === 'H3') {
+      lastHeader = child;
+    }
+    else {
+      items.push(child);
+      if (lastHeader) {
+        headers.set(child, lastHeader);
+      }
+    }
+  }
 
   const matchingItems = new Reactive(items);
 
   const paginator = makePaginator(matchingItems, perPage);
 
   reactTo({ visibleItems: paginator.visibleItems }, deps => {
+    for (const header of headers.values()) {
+      header.hidden = true;
+    }
+
     for (const item of items) {
-      item.hidden = !deps.visibleItems.val.includes(item);
+      const visible = deps.visibleItems.val.includes(item);
+      item.hidden = !visible;
+      if (visible) {
+        const header = headers.get(item);
+        if (header) {
+          header.hidden = false;
+        }
+      }
     }
   });
 
