360b7af68048bd5c700b01ba2792831217e9f2cf
Steven Degutis <sbdegutis@gmail.com>
2023-11-14 09:21:07 -0600

    Fix read-online page.

--- site/books/read-on-web/read-book.html.tsx
+++ site/books/read-on-web/read-book.html.tsx
@@ -14,50 +14,47 @@ export default allBooks.filter(book => book.data.complete).map(book => {
       <Spaced>
         <SplitColumn>
 
-          <Typography>
-
-            <link rel="stylesheet" href='/css/book.css' />
-
-            <h2>{book.data.title}</h2>
-            <p class="subtitle">{book.data.subtitle}</p>
-            <p>By <span class="author">{book.data.author}</span></p>
-            <p><Rating n={book.data.rating} /></p>
-            {markdown.render(book.content)}
-
-            <hr />
-
-            {orderedSnippets.map((bookSnippet, i) => <>
-              <div class='chapter' id={`snippet-${bookSnippet.slug}`}>
-                <h3 class='chapter-header'>
-                  Chapter {i + 1} &mdash; { }
-                  <a href={bookSnippet.route}>
-                    {bookSnippet.renderedTitle}
-                  </a>
-                </h3>
-                {bookSnippet.renderedBody}
-                <hr />
-              </div>
-            </>)}
-
-          </Typography>
+          <link rel="stylesheet" href='/css/page/read-book.css' />
 
           <div>
 
-            <ul class="readonline-chapters">
-              <h3>Chapter Index</h3>
+            <Typography>
+              <h2>{book.data.title}</h2>
+              <p class="subtitle">{book.data.subtitle}</p>
+              <p>By <span class="author">{book.data.author}</span></p>
+              <p><Rating n={book.data.rating} /></p>
+              {markdown.render(book.content)}
+            </Typography>
 
+            <hr />
+
+            <h3>Chapter Index</h3>
+            <div class="readonline-chapters">
               {orderedSnippets.map((bookSnippet, i) => <>
-                <li>
-                  <p>
-                    Ch.{i + 1} { }
-                    <a href={`#snippet-${bookSnippet.slug}`}>
+                <span>Ch.{i + 1}</span>
+                <a href={`#snippet-${bookSnippet.slug}`}>
+                  {bookSnippet.renderedTitle}
+                </a>
+              </>)}
+            </div>
+
+          </div>
+
+          <div>
+            <Typography>
+              {orderedSnippets.map((bookSnippet, i) => <>
+                <div class='chapter' id={`snippet-${bookSnippet.slug}`}>
+                  <h3 class='chapter-header'>
+                    Chapter {i + 1} &mdash; { }
+                    <a href={bookSnippet.route}>
                       {bookSnippet.renderedTitle}
                     </a>
-                  </p>
-                </li>
+                  </h3>
+                  {bookSnippet.renderedBody}
+                  <hr />
+                </div>
               </>)}
-            </ul>
-
+            </Typography>
           </div>
 
         </SplitColumn>
