f5d535ac048929ca4c3ec9ab4c7fcd51504e74bd
Steven Degutis <sbdegutis@gmail.com>
2023-11-15 11:59:43 -0600

    Paginate article list.

--- site/components/articles-list.tsx
+++ site/components/articles-list.tsx
@@ -1,16 +1,31 @@
 import { allArticles } from "../model/articles.js";
 import { formatDate } from "../shared/format-date.js";
+import { LoadingParagraph } from "../shared/loading.js";
+import { PaginatorLoading } from "./paginator.js";
 
 export const ArticlesList = () => <>
+  <script type='module' src='/scripts/paginate.js' />
   <div>
     <h2>All Articles</h2>
-    <ul>
-      {allArticles.map(article => <>
-        <li>
-          <a class="title" href={article.route}>{article.data.title}</a><br />
-          <small>{article.mins} min &bull; {formatDate(article.date)}</small>
-        </li>
-      </>)}
-    </ul>
+    <div data-paginate='7'>
+      <div>
+        <PaginatorLoading />
+        <LoadingParagraph lines={2} />
+        <LoadingParagraph lines={2} />
+        <LoadingParagraph lines={2} />
+        <LoadingParagraph lines={2} />
+        <LoadingParagraph lines={2} />
+        <LoadingParagraph lines={2} />
+        <LoadingParagraph lines={2} />
+      </div>
+      <div hidden>
+        {allArticles.map(article => <>
+          <p>
+            <a class="title" href={article.route}>{article.data.title}</a><br />
+            <small>{article.mins} min &bull; {formatDate(article.date)}</small>
+          </p>
+        </>)}
+      </div>
+    </div>
   </div>
 </>;
