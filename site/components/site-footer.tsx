e12363c5f3adef896eeee37c52f6a0c11d7a6b32
Steven Degutis <sbdegutis@gmail.com>
2023-11-19 10:59:21 -0600

    Show current year on every page load in footer.

--- site/components/site-footer.tsx
+++ site/components/site-footer.tsx
@@ -1,6 +1,8 @@
+const currentYearScript = <script>{`document.write(new Date().getFullYear())`}</script>;
+
 export const SiteFooter: JSX.Component = (attrs, children) => <>
   <footer id='site-footer'>
     <link rel="stylesheet" href='/css/components/site-footer.css' />
-    <p><span>2020-{new Date().getFullYear()} <a href="mailto:sbdegutis@gmail.com">Steven Degutis</a> &copy; All Rights Reserved</span></p>
+    <p><span>2020-{currentYearScript} <a href="mailto:sbdegutis@gmail.com">Steven Degutis</a> &copy; All Rights Reserved</span></p>
   </footer>
 </>;
