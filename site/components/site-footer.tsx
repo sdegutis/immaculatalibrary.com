bfb78b3cc5e70b68b33aa2860e2b3dc183e7130d
Steven Degutis <sbdegutis@gmail.com>
2024-04-06 15:12:27 -0500

    Test

--- site/components/site-footer.tsx
+++ site/components/site-footer.tsx
@@ -3,6 +3,6 @@ const currentYearScript = <script>{`document.write(new Date().getFullYear())`}</
 export const SiteFooter: JSX.Component<{ thin?: boolean }> = (attrs, children) => <>
   <footer id='site-footer' class={attrs.thin ? 'thin' : ''}>
     <link rel="stylesheet" href='/css/components/site-footer.css' />
-    <p><span>2020-{currentYearScript} <a href="mailto:sbdegutis@gmail.com">Steven Degutis</a> &copy; All Rights Reserved</span></p>
+    <p><span>2020 to {currentYearScript} <a href="mailto:sbdegutis@gmail.com">Steven Degutis</a> &copy; All Rights Reserved</span></p>
   </footer>
 </>;
