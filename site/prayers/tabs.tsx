638529f1347bf774441536e4d2eade2458d26128
Steven Degutis <sbdegutis@gmail.com>
2024-05-25 08:27:10 -0500

    Hmm...

--- site/prayers/tabs.tsx
+++ site/prayers/tabs.tsx
@@ -13,13 +13,13 @@ for (const a of tabs) {
     };
 }
 
-i = 0;
-const today = new Date().getDay();
-for (const day of document.querySelectorAll<HTMLElement>('.show-today')) {
-    if (today !== i++) {
-        day.remove();
-    }
-}
+// i = 0;
+// const today = new Date().getDay();
+// for (const day of document.querySelectorAll<HTMLElement>('.show-today')) {
+//     if (today !== i++) {
+//         day.remove();
+//     }
+// }
 
 for (const slideshow of document.querySelectorAll<HTMLDivElement>('.slideshow')) {
     for (let i = 1; i < slideshow.children.length; i++) {
