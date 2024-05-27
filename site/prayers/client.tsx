73e15d191d402b3c14a8b35ce8afa2720c936357
Steven Degutis <sbdegutis@gmail.com>
2024-05-27 10:06:12 -0500

    Keyboard shortcuts.

--- site/prayers/client.tsx
+++ site/prayers/client.tsx
@@ -1,37 +1,84 @@
-let i = 0;
-const tabs = document.querySelectorAll<HTMLAnchorElement>('#tabs-names a');
-for (const a of tabs) {
-    const child = document.getElementById('tabs-bodies')?.children[i++];
+const today = new Date().getDay();
+for (const [i, day] of Object.entries(document.querySelectorAll<HTMLElement>('.show-today'))) {
+    if (today !== +i) {
+        day.closest('.panel')?.remove();
+    }
+}
+
+interface Tab {
+    firstPanel: Panel;
+}
+
+class Panel {
+    public prev?: Panel | undefined;
+    public next?: Panel | undefined;
+    constructor(
+        public panelDiv: HTMLDivElement,
+        public panelBodyDiv: HTMLDivElement,
+    ) { }
+    focus() {
+        this.panelDiv.scrollIntoView({ behavior: 'smooth' });
+        this.panelBodyDiv.focus({ preventScroll: true });
+    }
+}
+
+const tabs: Tab[] = [];
+
+for (const slideshow of document.querySelectorAll<HTMLDivElement>('.slideshow')) {
+    let lastPanel: Panel | undefined;
+
+    for (const panelDiv of slideshow.querySelectorAll<HTMLDivElement>('.panel')) {
+        const panelBodyDiv = panelDiv.querySelector<HTMLDivElement>('.panel-body')!;
+
+        const panel = new Panel(panelDiv, panelBodyDiv);
+        panel.prev = lastPanel;
+
+        if (lastPanel) lastPanel.next = panel;
+        lastPanel = panel;
+
+        panelBodyDiv.setAttribute('tabindex', '0');
+        panelBodyDiv.onkeydown = (e) => {
+            if (e.key === 'ArrowLeft') {
+                e.preventDefault();
+                panel.prev?.focus();
+            }
+            else if (e.key === 'ArrowRight') {
+                e.preventDefault();
+                panel.next?.focus();
+            }
+        };
+    }
+
+    let firstPanel = lastPanel!;
+    while (firstPanel.prev) firstPanel = firstPanel.prev;
+
+    for (let panel: Panel = firstPanel; panel; panel = panel.next!) {
+        if (panel.prev) panel.panelDiv.append(<PageChanger to={panel.prev} side='left '>&lsaquo;</PageChanger>);
+        if (panel.next) panel.panelDiv.append(<PageChanger to={panel.next} side='right'>&rsaquo;</PageChanger>);
+    }
+
+    const tab: Tab = { firstPanel };
+    tabs.push(tab);
+}
+
+
+
+const tabButtons = document.querySelectorAll<HTMLAnchorElement>('#tabs-names a');
+for (const [i, a] of Object.entries(tabButtons)) {
     a.onclick = (e) => {
         e.preventDefault();
-        child?.scrollIntoView({ behavior: 'smooth' });
+        const tab = tabs[+i]!;
+        console.log(tab)
+        tab.firstPanel.focus();
 
-        for (const a of tabs) {
+        for (const a of tabButtons) {
             a.classList.remove('active');
         }
         a.classList.add('active');
     };
 }
 
-i = 0;
-const today = new Date().getDay();
-for (const day of document.querySelectorAll<HTMLElement>('.show-today')) {
-    if (today !== i++) {
-        day.closest('.panel')?.remove();
-    }
-}
-
-for (const slideshow of document.querySelectorAll<HTMLDivElement>('.slideshow')) {
-    for (let i = 1; i < slideshow.children.length; i++) {
-        const a = slideshow.children[i - 1] as HTMLElement;
-        const b = slideshow.children[i] as HTMLElement;
-
-        b.append(<PageChanger to={a} side='left'>&lsaquo;</PageChanger> as HTMLElement);
-        a.append(<PageChanger to={b} side='right'>&rsaquo;</PageChanger> as HTMLElement);
-    }
-}
-
-function PageChanger(attrs: { to: HTMLElement, side: 'left' | 'right' }, children: any) {
+function PageChanger(attrs: { to: Panel, side: 'left ' | 'right' }, children: any) {
     const button = (
         <button class='page-changer' style={`${attrs.side}: 0`}>
             {children}
@@ -39,9 +86,9 @@ function PageChanger(attrs: { to: HTMLElement, side: 'left' | 'right' }, childre
     ) as HTMLButtonElement;
     button.onclick = (e) => {
         e.preventDefault();
-        attrs.to.scrollIntoView({ behavior: 'smooth' });
+        attrs.to.focus();
     };
     return button;
 }
 
-document.querySelector<HTMLElement>('.focus')?.scrollIntoView();
+tabs[0]?.firstPanel.focus();
