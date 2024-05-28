2e2282cbc06c7880b74d282c705ab025cab1f6d8
Steven Degutis <sbdegutis@gmail.com>
2024-05-28 07:04:43 -0500

    Favicon 2.

--- site/sidebar/index.html.tsx
+++ site/sidebar/index.html.tsx
@@ -9,7 +9,7 @@ export default <>
       <title>Sidebar</title>
       <link rel="stylesheet" href="./style.css" />
       <script src="https://unpkg.com/romcal@1.3.0/dist/romcal.bundle.min.js"></script>
-      <link rel="icon" type="image/png" sizes="32x32" href='./app.ico' />
+      <link rel="icon" type="image/png" sizes="32x32" href='./favicon.ico' />
     </head>
 
     <body>
