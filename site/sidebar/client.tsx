a75fc3608784dd558b76b939e450d9a046b6db1a
Steven Degutis <sbdegutis@gmail.com>
2024-06-01 10:39:27 -0500

    Better weather.

--- site/sidebar/client.tsx
+++ site/sidebar/client.tsx
@@ -10,25 +10,12 @@ document.getElementById('root')!.append(<>
 
     <div id="info">
       <TimeArea />
-      <div>
-        <div id="temprow">
-          <div id="temperature">87 Fº</div>
-          <img id="weather-icon" />
-          <div id="weather-glimpse">
-            <div id="weather-glimpse-desc"></div>
-            <div id="weather-glimpse-max"></div>
-            <div id="weather-glimpse-min"></div>
-          </div>
-        </div>
-        <div id="weather-full">It's gonna rain or somethin.</div>
-      </div>
+      <Weather />
       <FeastDay />
     </div>
 
   </div>
 </>);
-
-
 
 function FeastDay() {
   const feastDayEl = <div id="feastday">Saint of the Day</div> as HTMLDivElement;
@@ -46,63 +33,30 @@ function FeastDay() {
   return feastDayEl;
 }
 
-// Weather
-const params = new URLSearchParams(location.search);
-const apiKey = '0adc97ca15785c49faffe4b60d623350';
-const weatherQuery = 'lon=-88.4487&lat=42.3147';
-const url = `https://api.openweathermap.org/data/2.5/onecall?appid=${apiKey}&${weatherQuery}&exclude=minutely&units=imperial`;
-const weatherIconEl = document.getElementById('weather-icon') as HTMLImageElement;
-const temperatureEl = document.getElementById('temperature')!;
-const weatherGlimpseDescEl = document.getElementById('weather-glimpse-desc')!;
-const weatherGlimpseMinEl = document.getElementById('weather-glimpse-min')!;
-const weatherGlimpseMaxEl = document.getElementById('weather-glimpse-max')!;
-const weatherFullEl = document.getElementById('weather-full')!;
-const hourFormatter = new Intl.DateTimeFormat('en', { hour12: true, hour: 'numeric' });
-function updateWeather() {
-  fetch(url).then(res => res.json()).then(json => {
-    weatherIconEl.src = `http://openweathermap.org/img/wn/${json.current.weather[0].icon}@2x.png`;
-    const temps = Object.values<any>(json.daily[1].feels_like);
-    temperatureEl.innerText = `${Math.round(json.current.feels_like)} Fº`;
-    weatherGlimpseDescEl.innerText = json.current.weather[0].main;
-    weatherGlimpseMaxEl.innerText = `High: ${Math.round(Math.max(...temps))}`;
-    weatherGlimpseMinEl.innerText = `Low:  ${Math.round(Math.min(...temps))}`;
-    weatherFullEl.innerText = makeDescription(json.hourly);
-    // TODO: handle alerts
-    // json.alerts may or may not exist
-    // If it does, it looks like:
-    // https://openweathermap.org/api/one-call-api#hist_parameter (sample above it)
-  });
-}
-updateWeather();
-setInterval(updateWeather, 1000 * 60 * 3 /* 3 minutes */);
-function makeDescription(list: any[]) {
-  list = list.slice(0, 12).map(item => item.weather[0].description);
-  list.push(null);
-  let hour = 0;
-  let count = 0;
-  let current = '';
-  const overall = [];
-  for (const item of list) {
-    if (item !== current) {
-      let desc;
-      if (current.match(/rain|snow|storm/i)) {
-        const now = new Date();
-        now.setHours(now.getHours() + hour);
-        const start = hourFormatter.format(now);
-        now.setHours(now.getHours() + count);
-        const end = hourFormatter.format(now);
-        desc = `${current} from ${start} to ${end}`;
-      }
-      else {
-        const hours = count > 1 ? 'hours' : 'hour';
-        desc = `${count} ${hours} of ${current}`;
-      }
-      overall.push(desc);
-      current = item;
-      hour += count;
-      count = 0;
-    }
-    count++;
+function Weather() {
+  const node = <div>
+    <div id="temprow">
+      <div id="temperature">87 Fº</div>
+      <img id="weather-icon" />
+    </div>
+    <div id="weather-full">It's gonna rain or somethin.</div>
+  </div> as HTMLElement;
+
+  updateWeather();
+  setInterval(updateWeather, 1000 * 60 * 60);
+
+  function updateWeather() {
+    navigator.geolocation.getCurrentPosition(async pos => {
+      const { latitude, longitude } = pos.coords;
+      const points = await fetch(`https://api.weather.gov/points/${latitude},${longitude}`).then(res => res.json());
+      const forecast = await fetch(points.properties.forecast).then(res => res.json());
+      const now = forecast.properties.periods[0];
+
+      node.querySelector<HTMLImageElement>('#weather-icon')!.src = now.icon;
+      node.querySelector<HTMLElement>('#temperature')!.innerText = `${now.temperature} ${now.temperatureUnit}°`;
+      node.querySelector<HTMLElement>('#weather-full')!.innerText = now.detailedForecast;
+    });
   }
-  return overall.slice(1).join(', ');
+
+  return node;
 }
