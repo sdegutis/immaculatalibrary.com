c43f91c88424b6c92f0be27a6c39aee2c1b171aa
Steven Degutis <sbdegutis@gmail.com>
2024-06-01 13:27:34 -0500

    Dunno.

--- site/resources.html.tsx
+++ site/resources.html.tsx
@@ -12,19 +12,25 @@ export default <>
           <h2>Resources</h2>
 
           <h3>Bibles</h3>
-          <p><a href="https://www.amazon.com/dp/0898708338">Hardcover (RSV-CE2)</a></p>
-          <p><a href="https://www.truthandlifeapp.com/">Audiobook (RSV-CE New Testament)</a></p>
-          <p><a href="https://catenabible.com/">Catena Bible</a></p>
+          <ul>
+            <li><a href="https://www.amazon.com/dp/0898708338">Hardcover (RSV-CE2)</a></li>
+            <li><a href="https://www.truthandlifeapp.com/">Audiobook (RSV-CE New Testament)</a></li>
+            <li><a href="https://catenabible.com/">Catena Bible</a></li>
+          </ul>
 
           <h3>Live streams</h3>
-          <p><a href='https://mass-online.org/daily-holy-mass-live-online/'>Online Masses</a></p>
-          <p><a href='https://www.youtube.com/watch?v=rz5gektkF0o'>Online Adoration</a></p>
-          <p><a href='/sidebar/'>Sidebar</a> (install as an app)</p>
+          <ul>
+            <li><a href='https://mass-online.org/daily-holy-mass-live-online/'>Online Masses</a></li>
+            <li><a href='https://www.youtube.com/watch?v=rz5gektkF0o'>Online Adoration</a></li>
+            <li><a href='/sidebar/'>Sidebar</a> (install as an app)</li>
+          </ul>
 
           <h3>Prayers</h3>
-          <p><a href='/prayers/'>Daily Prayers</a></p>
-          <p><a href='http://auxiliumchristianorum.org/'>Auxilium Christianorum</a></p>
-          <p><a href='https://www.catholicexorcism.org/'>CatholicExorcism.org</a></p>
+          <ul>
+            <li><a href='/prayers/'>Daily Prayers</a></li>
+            <li><a href='http://auxiliumchristianorum.org/'>Auxilium Christianorum</a></li>
+            <li><a href='https://www.catholicexorcism.org/'>CatholicExorcism.org</a></li>
+          </ul>
 
         </Typography>
       </CenteredColumn>
