d346cb366c00c5ce1850138e8a0c81b64bfe1267
Steven Degutis <sbdegutis@gmail.com>
2024-06-01 14:03:45 -0500

    Email.

--- site/components/site-footer.tsx
+++ site/components/site-footer.tsx
@@ -3,6 +3,6 @@ const currentYearScript = <script>{`document.write(new Date().getFullYear())`}</
 export const SiteFooter: JSX.Component<{ thin?: boolean }> = (attrs, children) => <>
   <footer id='site-footer' class={attrs.thin ? 'thin' : ''}>
     <link rel="stylesheet" href='/css/components/site-footer.css' />
-    <p><span>2020-{currentYearScript} &copy; All Rights Reserved</span></p>
+    <p><span>2020-{currentYearScript} &copy; All Rights Reserved &bullet; <a href='mailto:contact@genzbiblestories.com'>Email</a></span></p>
   </footer>
 </>;
