12269e527b939d9cf6cec16f0790b889cdc7829d
Steven Degutis <sbdegutis@gmail.com>
2024-06-02 09:11:05 -0500

    Shorter morning prayers.

--- site/prayers/index.html.tsx
+++ site/prayers/index.html.tsx
@@ -13,7 +13,6 @@ export default <>
           <GloryBe />
           <PreciousBlood />
           <SaintMichael />
-          <Memorare />
           <HolyFamilyPrayer />
         </Slideshow>,
       "Noon":
