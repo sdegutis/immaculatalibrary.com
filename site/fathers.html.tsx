8cdc7825e305e99764f5ce7a0e41f679c9a7b6db
Steven Degutis <sbdegutis@gmail.com>
2024-06-09 13:25:37 -0500

    Update title.

--- site/fathers.html.tsx
+++ site/fathers.html.tsx
@@ -1,16 +1,25 @@
-import { ArticlesList } from "./components/articles-list.js";
 import { Spaced, SplitColumn } from "./components/column.js";
 import { TypicalPage } from "./components/page.js";
-import { Typography } from "./components/typography.js";
 
 export default <>
-  <TypicalPage title="Articles" image='/img/page/articles.jpg'>
+  <TypicalPage title="Fathers of the Church" image='/img/page/articles.jpg'>
 
     <Spaced>
       <SplitColumn>
 
-        <span>Commentary on the Gospels by the Fathers of the Church, made easily searchable. (Catena Bible is missing quite a few quotes, plus that site is made by some orthodoxes.)</span>
-        <span>Coming soon.</span>
+        <div>
+          <p>
+            <a href='/books/catena-aurea.html'>The Catena Aurea</a> contains commentary on the Gospels
+            by the Fathers of the Church. This page lets you search through the digitized pages.
+          </p>
+          <p>(Catena Bible is missing quite a few quotes, plus that site is made by some orthodoxes.)</p>
+        </div>
+
+        <div>
+          <p>
+            Coming soon.
+          </p>
+        </div>
 
       </SplitColumn>
     </Spaced>
