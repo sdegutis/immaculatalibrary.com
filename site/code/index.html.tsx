52c0d605b53a9dcb5f211562bcdd8f4bf0fe2214
sdegutis <sbdegutis@gmail.com>
2024-06-11 08:13:19 -0500

    Basic FAQ about code.

--- site/code/index.html.tsx
+++ site/code/index.html.tsx
@@ -3,7 +3,35 @@ import martel from '../fonts/martel/';
 
 export default <>
   <Html>
-    Coming soon.
+    <h1>Front-end framework that runs Immaculata Library</h1>
+    <p>I wrote this to solve a few problems for myself:</p>
+    <ul>
+      <li>Performance:
+        <ul>
+          <li>Transform 1300+ Markdown files quickly</li>
+          <li>Build the whole site in ~900ms</li>
+          <li>Rebuild single pages in ~90ms</li>
+          <li>Avoid the slowness of SSG-side tools</li>
+          <li>Not be tied to any browser-side frameworks</li>
+        </ul>
+      </li>
+      <li>Code:
+        <ul>
+          <li>Use JSX as the view/templating language SSG-side</li>
+          <li>Use the DOM directly browser-side</li>
+          <li>Have the entire SSG be a simple TypeScript app</li>
+          <li>Import data files via TypeScript <code>import</code> statements</li>
+        </ul>
+      </li>
+      <li>JSX:
+        <ul>
+          <li>Render JSX in both SSG and browser for SEO</li>
+          <li>Make JSX browser-side transform to <code>document.createElement</code></li>
+          <li>Make JSX SSG-side transform to plain strings</li>
+          <li>Have shared JSX-producing functions for SSG and browser side</li>
+        </ul>
+      </li>
+    </ul>
   </Html>
 </>;
 
