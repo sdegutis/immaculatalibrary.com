ac91d4334d4143677b08526e085c913b9f972af8
sdegutis <sbdegutis@gmail.com>
2024-06-10 20:00:00 -0500

    Less hacky waiter.

--- site/scripts/monaco.tsx
+++ site/scripts/monaco.tsx
@@ -11,7 +11,13 @@ export function loadMonaco(): Promise<Monaco> {
       }
     }
 
-    let count = 3;
+    const scripts = [
+      "https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.43.0/min/vs/loader.min.js",
+      "https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.43.0/min/vs/editor/editor.main.nls.js",
+      "https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.43.0/min/vs/editor/editor.main.js",
+    ];
+
+    let count = scripts.length;
     function loaded() {
       count--;
       if (count === 0) {
@@ -22,9 +28,9 @@ export function loadMonaco(): Promise<Monaco> {
     (window as any).require = { paths: { 'vs': 'https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.43.0/min/vs' } };
     document.body.append(<>
       <link rel="stylesheet" data-name="vs/editor/editor.main" href="https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.43.0/min/vs/editor/editor.main.min.css" />
-      <script onload={loaded} src="https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.43.0/min/vs/loader.min.js" />
-      <script onload={loaded} src="https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.43.0/min/vs/editor/editor.main.nls.js" />
-      <script onload={loaded} src="https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.43.0/min/vs/editor/editor.main.js" />
+      {scripts.map(src =>
+        <script onload={loaded} src={src} />
+      )}
     </>);
   });
 }
