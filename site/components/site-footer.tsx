dff5e417844dcc53f2806828b6da8230135c62b5
sdegutis <sbdegutis@gmail.com>
2024-06-15 13:39:52 -0500

    Source link.

--- site/components/site-footer.tsx
+++ site/components/site-footer.tsx
@@ -1,8 +1,11 @@
 const currentYearScript = <script>{`document.write(new Date().getFullYear())`}</script>;
+const Copyright = () => <>2020-{currentYearScript} &copy; All Rights Reserved</>;
+const Email = () => <a href='mailto:sbdegutis@gmail.com'>Email</a>;
+const Source = () => <a href='https://github.com/sdegutis/immaculatalibrary.com'>Source code</a>;
 
 export const SiteFooter: JSX.Component<{ thin?: boolean }> = (attrs, children) => <>
   <footer id='site-footer' class={attrs.thin ? 'thin' : ''}>
     <link rel="stylesheet" href='/css/components/site-footer.css' />
-    <p><span>2020-{currentYearScript} &copy; All Rights Reserved &bullet; <a href='mailto:contact@genzbiblestories.com'>Email</a></span></p>
+    <p><span><Copyright /> &bullet; <Email /> &bullet; <Source /></span></p>
   </footer>
 </>;
