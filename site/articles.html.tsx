ebb53304670ea8b69aa1bc63e9b18047aa27c591
sdegutis <sbdegutis@gmail.com>
2024-06-19 15:21:18 -0500

    Add new article button.

--- site/articles.html.tsx
+++ site/articles.html.tsx
@@ -2,6 +2,7 @@ import { Typography } from "./components/$typography.js";
 import { ArticlesList } from "./components/articles-list.js";
 import { Spaced, SplitColumn } from "./components/column.js";
 import { TypicalPage } from "./components/page.js";
+import { isDev } from "./util/helpers.js";
 
 export default <>
   <TypicalPage title="Articles" image='/img/page/articles.jpg' page="Articles">
@@ -12,6 +13,9 @@ export default <>
         <Typography>
           <h2>Articles of Immaculata Library</h2>
           <p>This is where I have rambled a lot.</p>
+          {isDev && <>
+            <p><a href='/admin/articles/new.html'>New article</a></p>
+          </>}
         </Typography>
 
         <ArticlesList />
