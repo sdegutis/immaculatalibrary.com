ab7ea7dbfe76cf270622c5105283312a1c078f47
sdegutis <sbdegutis@gmail.com>
2024-06-19 15:48:10 -0500

    Fix resources page.

--- site/resources.html.tsx
+++ site/resources.html.tsx
@@ -3,30 +3,31 @@ import { CenteredColumn, Spaced } from './components/column.js';
 import { TypicalPage } from './components/page.js';
 
 export default <>
-  <TypicalPage title="Bible" image='/img/categories/classics-big.jpg' page='Resources'>
+  <TypicalPage title="Catholic Resources" image='/img/categories/classics-big.jpg' page='Resources'>
 
     <Spaced>
       <CenteredColumn>
         <Typography>
 
-          <h2>Resources</h2>
-
-          <h3>Live streams</h3>
+          <h2>Livestreams</h2>
           <ul>
             <li><a href='https://mass-online.org/daily-holy-mass-live-online/'>Online Masses</a></li>
             <li><a href='https://www.youtube.com/watch?v=rz5gektkF0o'>Online Adoration</a></li>
-            <li><a href='/sidebar/'>Sidebar</a> (install as an app)</li>
           </ul>
 
-          <h3>Other</h3>
-          <li><a href='https://www.catholicexorcism.org/'>CatholicExorcism.org</a></li>
-
-          <h3>Bibles</h3>
+          <h2>Bibles</h2>
           <ul>
-            <li><a href="https://www.amazon.com/dp/0898708338">Hardcover (RSV-CE2)</a></li>
-            <li><a href="https://www.truthandlifeapp.com/">Audiobook (RSV-CE New Testament)</a></li>
-            <li><a href="https://catenabible.com/">Catena Bible</a></li>
+            <li><a href="https://www.amazon.com/dp/0898708338">Buy Hardcover</a></li>
+            <li><a href="https://www.truthandlifeapp.com/">Audio Bible</a></li>
           </ul>
+
+          <h2>Apps</h2>
+          <ul>
+            <li><a href='/sidebar/'>Adoration Sidebar</a> (install as an app)</li>
+          </ul>
+
+          <h2>Blogs</h2>
+          <li><a href='https://www.catholicexorcism.org/'>CatholicExorcism.org</a></li>
 
         </Typography>
       </CenteredColumn>
