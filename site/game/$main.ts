1b9f3324a9047a6de904cdd99f871e490b9efa08
sdegutis <sbdegutis@gmail.com>
2024-06-23 19:08:50 -0500

    Organize.

--- site/game/$main.ts
+++ site/game/$main.ts
@@ -1,36 +1,23 @@
-export default 0;
+import { getFileText, getPico8Dir } from "./$files.js";
+
+// sarahs idea:
+//   i can place bombs that blow up certain bricks
+//   jane can pick up keys that open doors
+//   and sarah can push buttons that open bars
 
 const dir = await getPico8Dir();
 
 
 const text = await getFileText(dir, 'explore.p8');
 
-parsePico8File(text);
+const groups = parseGroups(text);
 
-function parsePico8File(text: string) {
-  const groups: Record<string, string[]> = Object.create(null);
-  let group = '';
-
-  for (const line of text.trim().split('\n')) {
-    if (line.startsWith('__')) {
-      group = line;
-      groups[group] = [];
-    }
-    else {
-      groups[group]?.push(line);
-    }
-  }
-
-  // const sprites:Sprite[] = [];
-
-  console.log(groups);
-}
+console.log(groups)
 
 
-
-// const canvas = <canvas width='1400' height='900' /> as HTMLCanvasElement;
-// document.body.append(canvas);
-
+const ctx = createDrawingContext();
+ctx.strokeStyle = '#f00';
+ctx.rect(10, 20, 30, 40);
 
 // console.log('hey')
 
@@ -71,78 +58,27 @@ function parsePico8File(text: string) {
 
 
 
-interface Database<T> {
-  get(): Promise<T>;
-  set(t: T): void;
-}
+function parseGroups(text: string) {
+  const groups: Record<string, string[]> = Object.create(null);
+  let group = '';
 
-async function getDb() {
-  return new Promise<Database<FileSystemDirectoryHandle>>(resolve => {
-    const req = indexedDB.open('game', 1);
-    const key = 'key';
-    req.onupgradeneeded = () => {
-      const db = req.result;
-      db.createObjectStore('thing2');
-    };
-    req.onsuccess = () => {
-      const db = req.result;
-
-      const store = () => {
-        const thing1 = db.transaction('thing2', 'readwrite');
-        const store = thing1.objectStore('thing2');
-        return store;
-      };
-
-      resolve({
-        async get() {
-          return new Promise(resolve => {
-            const val = store().get(key);
-            val.onsuccess = async () => resolve(val.result);
-          });
-        },
-        set(t) {
-          store().put(t, key);
-        },
-      });
-    };
-  });
-}
-
-async function getLocalDir() {
-  return new Promise<FileSystemDirectoryHandle>(resolve => {
-    const button = document.createElement('button');
-    button.textContent = 'Load file';
-    button.onclick = async e => {
-      e.preventDefault();
-      const dir = await window.showDirectoryPicker();
-      button.remove();
-      resolve(dir);
-    };
-    document.body.append(button);
-  });
-}
-
-async function getPico8Dir() {
-  const db = await getDb();
-  let dir = await db.get();
-  if (!dir) {
-    dir = await getLocalDir();
-    db.set(dir);
-  }
-  await dir.requestPermission();
-  return dir;
-}
-
-async function getFileText(dir: FileSystemDirectoryHandle, path: string) {
-  const parts = path.split('/');
-
-  let node = dir;
-  while (parts.length > 1) {
-    const subdir = parts.shift()!;
-    node = await dir.getDirectoryHandle(subdir);
+  for (const line of text.trim().split('\n')) {
+    if (line.startsWith('__')) {
+      group = line;
+      groups[group] = [];
+    }
+    else {
+      groups[group]?.push(line);
+    }
   }
 
-  const fileHandle = await node.getFileHandle(parts[0]!);
-  const text = await fileHandle.getFile().then(f => f.text());
-  return text;
+  return groups;
+}
+
+function createDrawingContext() {
+  const canvas = document.createElement('canvas');
+  canvas.width = 1400;
+  canvas.height = 900;
+  document.body.append(canvas);
+  return canvas.getContext('2d')!;
 }
