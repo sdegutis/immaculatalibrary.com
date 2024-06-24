9fe6dced4f495bd2e6c4005f1a79b6f4662e850b
sdegutis <sbdegutis@gmail.com>
2024-06-24 06:25:27 -0500

    Neat.

--- site/game/$main.ts
+++ site/game/$main.ts
@@ -9,16 +9,71 @@ const game1 = await loadP8('explore.p8');
 
 const ctx = createCanvas(1400, 900, 4);
 
-for (let y = 0; y < 64; y++) {
-  for (let x = 0; x < 128; x++) {
-    const spr = game1.map[y]![x]!;
-    if (spr > 0) {
-      const img = game1.sprites[spr]!;
-      ctx.putImageData(img, x * 8, y * 8);
+function runGameLoop(update: () => void) {
+  const framerate = 1000 / 30;
+  let from = +document.timeline.currentTime!;
+  const step = () => {
+    requestAnimationFrame(t => {
+      if (t - from >= framerate) {
+        update();
+        from = t;
+      }
+      step();
+    });
+  };
+  step();
+}
+
+let mx = 0;
+let my = 0;
+
+runGameLoop(() => {
+
+  const c = navigator.getGamepads()[0];
+  if (!c) return;
+
+  console.log(c.axes)
+
+  const [x1, y1, x2, y2] = c.axes as [number, number, number, number];
+
+  mx += x1;
+  my += y1;
+
+  mx += x2 * 3;
+  my += y2 * 3;
+
+  draw();
+
+});
+
+function draw() {
+  ctx.reset();
+
+  for (let y = 0; y < 64; y++) {
+    for (let x = 0; x < 128; x++) {
+      const spr = game1.map[y]![x]!;
+      if (spr > 0) {
+        const img = game1.sprites[spr]!;
+        ctx.putImageData(img, mx + x * 8, my + y * 8);
+      }
     }
   }
 }
 
+
+// function runGameLoop(update: () => void) {
+
+//   const start = +document.timeline.currentTime!;
+
+//   requestAnimationFrame(t => {
+
+
+
+//     // console.log(t - start);
+//     runGameLoop(update);
+//   });
+
+// }
 
 
 
