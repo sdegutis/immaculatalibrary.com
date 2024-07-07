e64a7e67298e3873c4af9c8b868dc93af901e057
sdegutis <sbdegutis@gmail.com>
2024-07-07 18:55:26 -0500

    Email.

--- site/components/site-footer.tsx
+++ site/components/site-footer.tsx
@@ -1,11 +1,10 @@
 const CurrentYear = () => <script>{`document.write(new Date().getFullYear())`}</script>;
 const Copyright = () => <>2020-<CurrentYear /> &copy; All Rights Reserved</>;
-const Email = () => <a href='mailto:sbdegutis@gmail.com'>Email</a>;
-const Source = () => <a href='https://github.com/sdegutis/immaculatalibrary.com'>Source code</a>;
+const Email = () => <a href='mailto:immaculatalibrary@gmail.com'>Email</a>;
 
 export const SiteFooter: JSX.Component<{ thin?: boolean }> = (attrs, children) => <>
   <footer id='site-footer' class={attrs.thin ? 'thin' : ''}>
     <link rel="stylesheet" href='/css/components/site-footer.css' />
-    <p><span><Copyright /> &bullet; <Email /> &bullet; <Source /></span></p>
+    <p><span><Copyright /> &bullet; <Email /></span></p>
   </footer>
 </>;
