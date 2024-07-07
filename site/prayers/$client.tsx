901388437e59e93e48081726394de62281a406c6
sdegutis <sbdegutis@gmail.com>
2024-07-07 07:19:44 -0500

    No need for lib dir now.

--- site/prayers/$client.tsx
+++ site/prayers/$client.tsx
@@ -1,5 +1,5 @@
-import { setupGamepads } from "./lib/$gamepad.js";
-import { setupTabs, tabs } from "./lib/$tab.js";
+import { setupGamepads } from "./$gamepad.js";
+import { setupTabs, tabs } from "./$tab.js";
 
 for (const el of document.querySelectorAll(`.show-today:not(.day-${new Date().getDay()})`)) {
   el.closest('.panel')?.remove();
