ac155ceb46a23fdf49a80e2081dde40724b6aeda
sdegutis <sbdegutis@gmail.com>
2024-08-06 15:04:49 -0500

    Cleaner types.

--- site/_libs.d.ts
+++ site/_libs.d.ts
@@ -1,14 +1,9 @@
 declare module 'https://cdn.jsdelivr.net/npm/markdown-it@13.0.2/+esm' {
-  import MarkdownIt = require('markdown-it/lib');
-  export default MarkdownIt;
-}
-
-declare module 'markdown-it' {
-  import MarkdownIt = require('markdown-it/lib');
+  import MarkdownIt from 'markdown-it';
   export default MarkdownIt;
 }
 
 declare module 'https://cdn.jsdelivr.net/npm/eases@1.0.8/+esm' {
-  const eases = await import('eases');
+  import eases from 'eases';
   export default eases;
 }
