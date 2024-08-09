2e533c8cc7f5b841d8578603d970eb4521715dae
sdegutis <sbdegutis@gmail.com>
2024-08-09 16:11:46 -0500

    Fix (but ugly).

--- site/@imlib/processor.ts
+++ site/@imlib/processor.ts
@@ -1,15 +1,14 @@
-import { defaultProcessors, processSite, SiteProcessor, skip } from "@imlib/core";
+import { defaultProcessors, Processor, processSite, SiteProcessor, skip } from "@imlib/core";
 import { makeSitemap } from "../sitemap.xml.js";
+import { isDev } from "../util/_helpers.js";
 
 export default ((files) => {
-  const processors = [...defaultProcessors];
-
-  if (!process.env['DEV']) {
-    processors.unshift([/^\/admin\//, skip]);
-  }
-
-  processors.push([/\.md$/, skip]);
-  processors.push([/_.*\.js$/, skip]);
+  const processors: Processor[] = [
+    isDev ? [/^\/admin\//, skip] as Processor : null,
+    [/\.md$/, skip] as Processor,
+    [/_.*\.js$/, skip] as Processor,
+    ...defaultProcessors,
+  ].filter(p => !!p);
 
   const out = processSite(files, processors);
   out.set('/sitemap.xml', makeSitemap(out.keys()));
