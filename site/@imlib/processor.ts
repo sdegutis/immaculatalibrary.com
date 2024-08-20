56621a075377a23d271570e83a5c40da7163f795
sdegutis <sbdegutis@gmail.com>
2024-08-20 11:35:08 -0500

    Simplify site processors.

--- site/@imlib/processor.ts
+++ site/@imlib/processor.ts
@@ -1,16 +1,12 @@
-import { defaultProcessors, Processor, processSite, SiteProcessor, skip } from "@imlib/core";
+import { processSite, SiteProcessor } from "@imlib/core";
 import { makeSitemap } from "../sitemap.xml.js";
 import { isDev } from "../util/_helpers.js";
 
 export default ((files) => {
-  const processors: Processor[] = [
-    !isDev ? [/^\/admin\//, skip] as Processor : null,
-    [/\.md$/, skip] as Processor,
-    [/_.*\.js$/, skip] as Processor,
-    ...defaultProcessors,
-  ].filter(p => !!p);
-
-  const out = processSite(files, processors);
+  const out = processSite(files
+    .filter(f => !f.path.startsWith('/admin/') || isDev)
+    .filter(f => !f.path.endsWith('.md'))
+    .filter(f => !f.path.match(/_.*\.js$/)));
   out.set('/sitemap.xml', makeSitemap(out.keys()));
   return out;
 }) as SiteProcessor;
