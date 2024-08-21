dec0813492d6c918c686646e99cabb27af666fff
sdegutis <sbdegutis@gmail.com>
2024-08-21 12:42:48 -0500

    Simpler article authoring.

--- site/admin/articles/new-article.tsx
+++ site/admin/articles/new-article.tsx
@@ -1,24 +1,12 @@
-import * as kv from 'idb-keyval';
 import { slugify } from '../util/helpers.js';
 
 const title = document.querySelector<HTMLInputElement>('[name=title]')!;
 const slug = document.querySelector<HTMLInputElement>('[name=slug]')!;
-const textarea = document.querySelector<HTMLTextAreaElement>('[name=content]')!;
-
-const saved = await kv.get('newarticle');
-if (saved) textarea.value = saved;
-
-const save = throttle(300, () => {
-  kv.set('newarticle', textarea.value);
-});
 
 title.oninput = () => slug.value = slugify(title.value);
-textarea.oninput = save;
 
-function throttle(ms: number, fn: () => void) {
-  let timer: NodeJS.Timeout;
-  return () => {
-    clearTimeout(timer);
-    timer = setTimeout(fn, ms);
-  };
-}
+window.addEventListener('beforeunload', (e) => {
+  if (!confirm('Lose progress?')) {
+    e.preventDefault();
+  }
+});
