60c9e78e30ffce1a8be5a87e75494b713798d7c7
sdegutis <sbdegutis@gmail.com>
2024-09-07 10:40:41 -0500

    Don't pass it on.

--- site/components/$typography.tsx
+++ site/components/$typography.tsx
@@ -1,6 +1,6 @@
-export const Typography = (attrs: { deindent?: boolean, children: any, [key: string]: any }) => <>
-  <div class={`typography${attrs.deindent ? ' deindent' : ''}`} {...attrs}>
+export const Typography = ({ children, deindent, ...attrs }: { deindent?: boolean, children: any, [key: string]: any }) => <>
+  <div class={`typography${deindent ? ' deindent' : ''}`} {...attrs}>
     <link rel="stylesheet" href='/css/components/typography.css' />
-    {attrs.children}
+    {children}
   </div>
 </>;
