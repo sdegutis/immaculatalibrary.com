f53249d03a398b351c3cf2c0d09462a8e762e148
sdegutis <sbdegutis@gmail.com>
2025-02-16 12:54:24 -0600

    Fixes.

--- site/moral-responsibility/morals.json.tsx
+++ site/moral-responsibility/morals.json.tsx
@@ -2,5 +2,5 @@ import { markdown } from '../util/_helpers.js';
 import files from './';
 
 const file = files.find(f => f.path.endsWith('morals.md'))!;
-const content = file.content.toString('utf8').replace(/\r/g, '');
-export default content.split(/\n\n/).map(s => markdown.render(s));
+const content = file.content.toString('utf8').trim().replace(/\r/g, '');
+export default content.split(/\n\n+/).map(s => markdown.render(s));
