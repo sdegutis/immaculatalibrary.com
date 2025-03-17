266e575985398425249a469de3b1e7d8ed78d8fe
sdegutis <sbdegutis@gmail.com>
2025-03-17 12:14:37 -0500

    Fix.

--- site/@imlib/processor.ts
+++ site/@imlib/processor.ts
@@ -3,36 +3,48 @@ import { extname } from "path";
 import { isDev } from "../components/admin.js";
 import { makeSitemap } from "../sitemap.js";
 
-const contentProcessors: Record<string, (s: any) => string> = {
-  '.html': hoistHtml,
-  '.json': JSON.stringify,
-};
-
-const importmap = `
-<script type="importmap">{
-  "imports": {
-    "markdown-it": "https://cdn.jsdelivr.net/npm/markdown-it@latest/+esm"
-  }
-}</script>
-`
-
-function hoistHtml(jsx: string) {
-  const hoisted = new Set<string>();
-  return (jsx
-    .replace(/<script .+?><\/script>|<link .+?>/g, (s, s2) => {
-      hoisted.add(s);
-      return '';
-    })
-    .replace(/<\/head>/, [...hoisted, importmap, '</head>'].join('')));
-}
-
-function processContent(content: any, ext: string) {
-  const fn = contentProcessors[ext];
-  return fn ? fn(content) : content;
-}
-
 export default (({ inFiles, outFiles }) => {
-  for (const file of inFiles) {
+
+  const files = [...inFiles]
+
+  // console.log(files
+  //   .map(f => f.module?.imports)
+  //   .filter(imps => imps !== undefined)
+  //   .flatMap(imps => [...imps])
+  //   .filter(imp => !imp.match(/(^[./])|(!$)|^immaculata$/))
+  //   .filter(imp => !builtinModules.includes(imp))
+  // )
+
+  const contentProcessors: Record<string, (s: any) => string> = {
+    '.html': hoistHtml,
+    '.json': JSON.stringify,
+  };
+
+  const importmap = `
+  <script type="importmap">{
+    "imports": {
+      "markdown-it": "https://cdn.jsdelivr.net/npm/markdown-it@latest/+esm",
+      "eases": "https://cdn.jsdelivr.net/npm/eases@latest/+esm"
+    }
+  }</script>
+  `
+
+  function hoistHtml(jsx: string) {
+    const hoisted = new Set<string>();
+    return (jsx
+      .replace(/<script .+?><\/script>|<link .+?>/g, (s, s2) => {
+        hoisted.add(s);
+        return '';
+      })
+      .replace(/<\/head>/, [...hoisted, importmap, '</head>'].join('')));
+  }
+
+  function processContent(content: any, ext: string) {
+    const fn = contentProcessors[ext];
+    return fn ? fn(content) : content;
+  }
+
+  for (const file of files) {
     if (
       (file.path.startsWith('/admin/') && !isDev) ||
       (file.path.startsWith('/data/')) ||
@@ -45,4 +57,5 @@ export default (({ inFiles, outFiles }) => {
   }
 
   outFiles.set('/sitemap.xml', makeSitemap(outFiles.keys()));
+
 }) as SiteProcessor;
