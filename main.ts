e402ec0fcf6f6b457eb0d32cfb0dd80fca8583d8
sdegutis <sbdegutis@gmail.com>
2025-03-30 16:11:26 -0500

    Assign options rather than in constructor.

--- main.ts
+++ main.ts
@@ -5,66 +5,68 @@ import { extname } from 'path'
 const isDev = process.argv[2] !== 'generate'
 const action = isDev ? immaculata.startDevServer : immaculata.generateFiles
 
-const runtime = new immaculata.Runtime({
-  processor: (files) => {
+const runtime = new immaculata.Runtime()
 
-    const outFiles: FsFile[] = []
+runtime.processor = (files) => {
 
-    // console.log(files
-    //   .map(f => f.module?.imports)
-    //   .filter(imps => imps !== undefined)
-    //   .flatMap(imps => [...imps])
-    //   .filter(imp => !imp.match(/(^[./])|(!$)|^immaculata$/))
-    //   .filter(imp => !builtinModules.includes(imp))
-    // )
+  const outFiles: FsFile[] = []
 
-    const contentProcessors: Record<string, (s: any) => string> = {
-      '.html': hoistHtml,
-      '.json': JSON.stringify,
-    }
+  // console.log(files
+  //   .map(f => f.module?.imports)
+  //   .filter(imps => imps !== undefined)
+  //   .flatMap(imps => [...imps])
+  //   .filter(imp => !imp.match(/(^[./])|(!$)|^immaculata$/))
+  //   .filter(imp => !builtinModules.includes(imp))
+  // )
 
-    const importmap = `
-    <script type="importmap">{
-      "imports": {
-        "markdown-it": "https://cdn.jsdelivr.net/npm/markdown-it@latest/+esm",
-        "eases": "https://cdn.jsdelivr.net/npm/eases@latest/+esm"
-      }
-    }</script>
-    `
-
-    function hoistHtml(jsx: string) {
-      const hoisted = new Set<string>()
-      return (jsx
-        .replace(/<script .+?><\/script>|<link .+?>/g, (s, s2) => {
-          hoisted.add(s)
-          return ''
-        })
-        .replace('<head>', `$&${importmap}`)
-        .replace(/<\/head>/, [...hoisted, '</head>'].join('')))
-    }
-
-    function processContent(content: any, ext: string) {
-      const fn = contentProcessors[ext]
-      return fn ? fn(content) : content
-    }
-
-    for (const file of files) {
-      if (
-        (file.path.startsWith('/admin/') && !isDev) ||
-        (file.path.startsWith('/data/')) ||
-        (file.path.startsWith('/model/'))
-      ) continue
-
-      for (const { path, content } of immaculata.processFile(file)) {
-        outFiles.push({ path, content: processContent(content, extname(path)) })
-      }
-    }
-
-    // outFiles.set('/sitemap.xml', makeSitemap(outFiles.keys()))
-
-    return outFiles
-
+  const contentProcessors: Record<string, (s: any) => string> = {
+    '.html': hoistHtml,
+    '.json': JSON.stringify,
   }
-})
+
+  const importmap = `
+  <script type="importmap">{
+    "imports": {
+      "markdown-it": "https://cdn.jsdelivr.net/npm/markdown-it@latest/+esm",
+      "eases": "https://cdn.jsdelivr.net/npm/eases@latest/+esm"
+    }
+  }</script>
+  `
+
+  function hoistHtml(jsx: string) {
+    const hoisted = new Set<string>()
+    return (jsx
+      .replace(/<script .+?><\/script>|<link .+?>/g, (s, s2) => {
+        hoisted.add(s)
+        return ''
+      })
+      .replace('<head>', `$&${importmap}`)
+      .replace(/<\/head>/, [...hoisted, '</head>'].join('')))
+  }
+
+  function processContent(content: any, ext: string) {
+    const fn = contentProcessors[ext]
+    return fn ? fn(content) : content
+  }
+
+  for (const file of files) {
+    if (
+      (file.path.startsWith('/admin/') && !isDev) ||
+      (file.path.startsWith('/data/')) ||
+      (file.path.startsWith('/model/'))
+    ) continue
+
+    for (const { path, content } of immaculata.processFile(file)) {
+      outFiles.push({ path, content: processContent(content, extname(path)) })
+    }
+  }
+
+  // outFiles.set('/sitemap.xml', makeSitemap(outFiles.keys()))
+
+  return outFiles
+
+}
+
+runtime.rebuildAll()
 
 action(runtime)
