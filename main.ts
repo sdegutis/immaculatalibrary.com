b20d7354380d712c57cdacb58114fc39b0f27a65
sdegutis <sbdegutis@gmail.com>
2025-03-30 17:14:24 -0500

    Organize.

--- main.ts
+++ main.ts
@@ -1,22 +1,6 @@
 import * as immaculata from 'immaculata'
 // import { makeSitemap } from './site/sitemap.js'
 
-const importmap = `
-<script type="importmap">{
-  "imports": {
-    "markdown-it": "https://cdn.jsdelivr.net/npm/markdown-it@latest/+esm",
-    "eases": "https://cdn.jsdelivr.net/npm/eases@latest/+esm"
-  }
-}</script>
-`
-
-function hoistHeaders(content: string) {
-  const hoisted = new Set<string>()
-  return (content
-    .replace(/<script .+?><\/script>|<link .+?>/g, (s, s2) => { hoisted.add(s); return '' })
-    .replace(/<\/head>/, [...hoisted, '</head>'].join('')))
-}
-
 const isDev = process.argv[2] !== 'generate'
 
 const runtime = new immaculata.Runtime()
@@ -28,6 +12,16 @@ runtime.processor = (files) => {
   files = files.flatMap(immaculata.processFile)
   files.filter(f => f.path.endsWith('.json')).forEach(f => f.content = JSON.stringify(f.content))
   files.filter(f => f.path.endsWith('.html')).forEach(f => f.content = hoistHeaders(f.content as string))
+
+  const importmap = `
+<script type="importmap">{
+  "imports": {
+    "markdown-it": "https://cdn.jsdelivr.net/npm/markdown-it@latest/+esm",
+    "eases": "https://cdn.jsdelivr.net/npm/eases@latest/+esm"
+  }
+}</script>
+`
+
   files.filter(f => f.path.endsWith('.html')).forEach(f => f.content = (f.content as string).replace('<head>', `$&${importmap}`))
   // outFiles.set('/sitemap.xml', makeSitemap(outFiles.keys()))
   return files
@@ -41,4 +35,11 @@ if (isDev) {
 }
 else {
   immaculata.generateFiles(runtime)
+}
+
+function hoistHeaders(content: string) {
+  const hoisted = new Set<string>()
+  return (content
+    .replace(/<script .+?><\/script>|<link .+?>/g, (s, s2) => { hoisted.add(s); return '' })
+    .replace(/<\/head>/, [...hoisted, '</head>'].join('')))
 }
