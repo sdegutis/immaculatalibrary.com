2464d9869852664cecaa5331eeec8f633501bc5c
sdegutis <sbdegutis@gmail.com>
2025-04-06 07:51:05 -0500

    Latest.

--- main.ts
+++ main.ts
@@ -1,19 +1,13 @@
-import immaculata from 'immaculata'
+import * as immaculata from 'immaculata'
 // import { makeSitemap } from './site/sitemap.js'
 
 const isDev = process.argv[2] !== 'generate'
 
-const runtime = new immaculata.Runtime()
+const tree = new immaculata.LiveTree('site', import.meta.url)
 
-runtime.processor = (files) => {
-  files = files.filter(f => !f.path.startsWith('/admin/') || isDev)
-  files = files.filter(f => !f.path.startsWith('/data/'))
-  files = files.filter(f => !f.path.startsWith('/model/'))
-  files = files.flatMap(immaculata.deprecated.processFile)
-  files.filter(f => f.path.endsWith('.json')).forEach(f => f.content = JSON.stringify(f.content))
-  files.filter(f => f.path.endsWith('.html')).forEach(f => f.content = hoistHeaders(f.content as string))
+tree.enableModules(immaculata.transformModuleJsxToStrings)
 
-  const importmap = `
+const importmap = `
 <script type="importmap">{
   "imports": {
     "markdown-it": "https://cdn.jsdelivr.net/npm/markdown-it@latest/+esm",
@@ -22,20 +16,62 @@ runtime.processor = (files) => {
 }</script>
 `
 
-  files.filter(f => f.path.endsWith('.html')).forEach(f => f.content = (f.content as string).replace('<head>', `$&${importmap}`))
-  // outFiles.set('/sitemap.xml', makeSitemap(outFiles.keys()))
-  return files
-}
+const singleDynFile = /\..+(?<ext>\.tsx?)$/
+const arrayDynFile = /\/.*(?<slug>\[.+\]).*\..+(?<ext>\.tsx?)$/
 
-runtime.loadTree()
+export const handlers = new Map<string, (body: string) => string>()
+
+async function processSite() {
+  return tree.processFiles(async files => {
+
+    if (!isDev) files.with('^/admin/')
+    files.with('^/data/')
+    files.with('^/model/')
+
+    await files.with(singleDynFile).doAsync(async file => {
+      const match = file.path.match(singleDynFile)!
+      const exports = await import('./site' + file.path)
+      const o = exports.default
+      file.path = file.path.slice(0, -match.groups!["ext"]!.length)
+      file.text = typeof o === 'string' ? o : JSON.stringify(o)
+    })
+
+    await files.with(arrayDynFile).doAsync(async file => {
+      const match = file.path.match(arrayDynFile)!
+      const exports = await import('./site' + file.path)
+      const array = exports.default as [string, any][]
+      files.del(file.path)
+
+      for (const [name, obj] of array) {
+        const filepath = file.path.replace(match.groups!["slug"]!, name)
+        files.add(filepath.slice(0, -match.groups!["ext"]!.length), '', obj)
+      }
+    })
+
+    files.with('\.html$').do(file => file.text = hoistHeaders(file.text))
+    files.with('\.html$').do(file => file.text = file.text.replace('<head>', `$&${importmap}`))
+
+  })
+}
 
 if (isDev) {
-  process.env['DEV'] = '1'
-  immaculata.startDevServer(runtime)
+  const server = new immaculata.DevServer(8080)
+  server.handlers = handlers
+  server.files = await processSite()
+
+  tree.watch({
+    ignored: (str) => str.endsWith('/site/api.d.ts')
+  }, async (paths) => {
+    const start = Date.now()
+    server.files = await processSite()
+    server.reload()
+    console.log(`Time: ${Date.now() - start} ms`)
+  })
 }
 else {
-  immaculata.generateFiles(runtime)
+  immaculata.generateFiles(await processSite())
 }
+
 
 function hoistHeaders(content: string) {
   const hoisted = new Set<string>()
